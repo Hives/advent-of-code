082a775a58ff4011b42e8ff65bd24aab1218838f
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-12-01 18:00:53 +0000

    Day 1: make recursion tail recursive

--- src/main/kotlin/days/day01.kt
+++ src/main/kotlin/days/day01.kt
@@ -2,9 +2,10 @@ package days
 import lib.combinations
 
 fun main() {
+    println("input length = ${input.size}")
     listOf(
         input.combinations(2).first { it.sum() == 2020 },
-        input.combinations(3).first { it.sum() == 2020 }
+        input.combinations(3).first { it.sum() == 2020 },
     ).forEach {
         println(it.joinToString(separator = " + ", postfix = " = ") + it.sum())
         println(it.joinToString(separator = " * ", postfix = " = ") + it.reduce { a, b -> a * b })
--- src/main/kotlin/lib/Combinations.kt
+++ src/main/kotlin/lib/Combinations.kt
@@ -1,11 +1,28 @@
 package lib
 
-fun List<Int>.combinations(n: Int): List<List<Int>> =
-        if (n == 1) this.map { listOf(it) }
-        else {
-            this.flatMapIndexed { index, i ->
-                val remainder = this.subList(index + 1, this.size)
-                // TODO: dangerous recursion!
-                remainder.combinations(n - 1).map { (it + i).sorted() }
+fun List<Int>.combinations(n: Int): List<List<Int>> {
+    fun addFromRemainderToSelection(
+        partialCombinations: List<PartialCombination>
+    ): List<PartialCombination> =
+        partialCombinations.flatMap { (selections, remainder) ->
+            remainder.mapIndexed { index, i ->
+                PartialCombination(
+                    selections = selections + i,
+                    remainder = remainder.subList(index + 1, remainder.size)
+                )
             }
         }
+
+    tailrec fun go(
+        partialCombinations: List<PartialCombination>,
+        iteration: Int
+    ): List<PartialCombination> =
+        if (iteration == 0) partialCombinations
+        else go(addFromRemainderToSelection(partialCombinations), iteration - 1)
+
+    return go(partialsFrom(this), n).map { it.selections }
+}
+
+private data class PartialCombination(val selections: List<Int>, val remainder: List<Int>)
+
+private fun partialsFrom(ints: List<Int>) = listOf(PartialCombination(emptyList(), ints))
