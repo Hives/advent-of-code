3efa48a15697c04852cb7fd8d5460e3f9a623632
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-12-02 14:16:24 +0000

    Day 2: refack

--- src/main/kotlin/days/day02.kt
+++ src/main/kotlin/days/day02.kt
@@ -6,8 +6,8 @@ fun main() {
     val input = Reader("day02.txt")
 
     val oldPasswordDetails = input.listOfOldPasswordDetails()
-    println("Number of valid passwords by old method: ${oldPasswordDetails.count { it.isValid }}")
+    println("Number of valid passwords by old method: ${oldPasswordDetails.count { it.validate() }}")
 
     val passwordDetails = input.listOfPasswordDetails()
-    println("Number of valid passwords by new method: ${passwordDetails.count { it.isValid }}")
+    println("Number of valid passwords by new method: ${passwordDetails.count { it.validate() }}")
 }
--- src/test/kotlin/lib/PasswordDetailsKtTest.kt
+++ src/test/kotlin/lib/PasswordDetailsKtTest.kt
@@ -12,25 +12,25 @@ internal class PasswordDetailsKtTest {
         @Test
         fun `neither position matches`() {
             val details = PasswordDetails("password", 'd', 0, 1)
-            assertThat(details.isValid).isFalse()
+            assertThat(details.validate()).isFalse()
         }
 
         @Test
         fun `both positions match`() {
             val details = PasswordDetails("password", 's', 2, 3)
-            assertThat(details.isValid).isFalse()
+            assertThat(details.validate()).isFalse()
         }
 
         @Test
         fun `first position matches`() {
             val details = PasswordDetails("password", 'a', 1, 3)
-            assertThat(details.isValid).isTrue()
+            assertThat(details.validate()).isTrue()
         }
 
         @Test
         fun `second position matches`() {
             val details = PasswordDetails("password", 's', 1, 3)
-            assertThat(details.isValid).isTrue()
+            assertThat(details.validate()).isTrue()
         }
     }
 
@@ -39,13 +39,25 @@ internal class PasswordDetailsKtTest {
         @Test
         fun `too few of character`() {
             val details = OldPasswordDetails("password", 'a', 2, 99)
-            assertThat(details.isValid).isFalse()
+            assertThat(details.validate()).isFalse()
         }
 
         @Test
         fun `too many of character`() {
-            val details = OldPasswordDetails("password", 's', 1, 1)
-            assertThat(details.isValid).isFalse()
+            val details = OldPasswordDetails("password", 's', 0, 1)
+            assertThat(details.validate()).isFalse()
+        }
+
+        @Test
+        fun `lower bound`() {
+            val details = OldPasswordDetails("password", 's', 2, 3)
+            assertThat(details.validate()).isTrue()
+        }
+
+        @Test
+        fun `upper bound`() {
+            val details = OldPasswordDetails("password", 's', 1, 2)
+            assertThat(details.validate()).isTrue()
         }
     }
 }
