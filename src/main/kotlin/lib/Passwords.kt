847b3175206afc78cb195be86ea2ac689696c714
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-12-02 19:05:50 +0000

    Day 2: don't need criteria superclass

--- src/main/kotlin/lib/Passwords.kt
+++ src/main/kotlin/lib/Passwords.kt
@@ -2,33 +2,29 @@ package lib
 
 fun String.extractPassword(): String = this.split(": ").last()
 
-fun String.extractOldCriteria(): Criteria = this
+fun String.extractOldCriteria() = this
     .extractCriteriaParts()
     .let { OldCriteria(it[2].single(), it[0].toInt(), it[1].toInt()) }
 
-fun String.extractNewCriteria(): Criteria = this
+fun String.extractNewCriteria() = this
     .extractCriteriaParts()
     .let { NewCriteria(it[2].single(), it[0].toInt() - 1, it[1].toInt() - 1) }
 
 private fun String.extractCriteriaParts() =
     this.split(": ").first().split("-", " ")
 
-sealed class Criteria {
-    abstract fun validate(password: String): Boolean
-}
-
-data class OldCriteria(
+class OldCriteria(
     val char: Char,
     val min: Int,
     val max: Int
-) : Criteria() {
-    override fun validate(password: String) = password.count { it == char } in min..max
+) {
+    fun validate(password: String) = password.count { it == char } in min..max
 }
 
-data class NewCriteria(
+class NewCriteria(
     val char: Char,
     val pos1: Int,
     val pos2: Int
-): Criteria() {
-    override fun validate(password: String) = (password[pos1] == char) xor (password[pos2] == char)
+) {
+    fun validate(password: String) = (password[pos1] == char) xor (password[pos2] == char)
 }
