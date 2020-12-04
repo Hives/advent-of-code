fbb51333edfb362d8c8b3e2af826c3ed3f886df6
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-12-04 10:48:00 +0000

    Day 4: refactor

--- src/main/kotlin/lib/Passport.kt
+++ src/main/kotlin/lib/Passport.kt
@@ -17,28 +17,27 @@ data class Passport(
 
     val isValid2: Boolean
         get() = listOf(
-            birthYear?.toIntOrNull()?.let { it in 1920..2002 } ?: false,
-            issueYear?.toIntOrNull()?.let { it in 2010..2020 } ?: false,
-            expirationYear?.toIntOrNull()?.let { it in 2020..2030 } ?: false,
+            birthYear.inRange(1920, 2002),
+            issueYear.inRange(2010, 2020),
+            expirationYear.inRange(2020, 2030),
             when {
                 height.isNullOrBlank() -> false
                 else -> when {
-                    height.endsWith("cm") ->
-                        height.replace("cm", "").toIntOrNull().let { it in 150..193 }
-                    height.endsWith("in") ->
-                        height.replace("in", "").toIntOrNull().let { it in 59..76 }
+                    height.endsWith("cm") -> height.replace("cm", "").inRange(150, 193)
+                    height.endsWith("in") -> height.replace("in", "").inRange(59, 76)
                     else -> false
                 }
             },
             when {
-                hairColor.isNullOrBlank() -> false
-                hairColor.length != 7 -> false
+                hairColor?.length != 7 -> false
                 !hairColor.startsWith("#") -> false
                 else -> hairColor.drop(1).all { "1234567890abcdef".contains(it) }
             },
             eyeColor?.let { listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(it) } ?: false,
             passportId?.let { it.length == 9 && it.toIntOrNull() != null } ?: false
         ).all { it }
+
+    private fun String?.inRange(low: Int, high: Int): Boolean = this?.toIntOrNull()?.let { it in low..high } ?: false
 
     companion object {
         fun from(input: String): Passport {
