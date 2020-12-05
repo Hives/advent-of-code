e22ee91b1deabfd486cbee0c10a5bf2cc69f2b96
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-12-05 16:30:07 +0000

    Day 5: refactor

--- src/main/kotlin/days/day05/SeatsBinary.kt
+++ src/main/kotlin/days/day05/SeatsBinary.kt
@@ -1,13 +1,9 @@
 package days.day05
 
-import kotlin.math.pow
-
-fun findSeatUsingBinaryStrings(input: String): Int {
-    val row = input.substring(0..6).replace('F','0').replace('B', '1')
-    val col = input.substring(7..9).replace('L','0').replace('R', '1')
-    return Integer.parseInt("$row$col", 2)
-}
-
-// no longer used, preserved for posterity 😞
-private fun String.binaryToInt(): Int =
-    this.reversed().mapIndexed { index, c -> c.toString().toInt() * 2.0.pow(index).toInt() }.sum()
+fun findSeatUsingBinaryStrings(input: String): Int =
+    input
+        .replace('F', '0')
+        .replace('B', '1')
+        .replace('L', '0')
+        .replace('R', '1')
+        .let { Integer.parseInt(it, 2) }
