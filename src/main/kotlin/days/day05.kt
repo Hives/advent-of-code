fa23c082e9843017c307d078765601798b14dd82
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-12-05 10:45:46 +0000

    Day 5: part 2

--- src/main/kotlin/days/day05.kt
+++ src/main/kotlin/days/day05.kt
@@ -1,6 +1,22 @@
 import lib.Reader
+import lib.Seat
 import lib.findSeat
 
 fun main() {
-    Reader("day05.txt").strings().map { findSeat(it).id }.maxOrNull().also { println(it) }
+    val seatIds = Reader("day05.txt").strings().map { findSeat(it) }.map { it.id }
+
+    seatIds.maxOrNull().also {
+        println("part 1: $it")
+    }
+
+    val allPossibleSeats = List(128) { it }.flatMap { row ->
+        List(8) { it }.map { col ->
+            Seat(row, col)
+        }
+    }
+    val allPossibleSeatIds = allPossibleSeats.map { it.id }
+    val missingSeatIds = allPossibleSeatIds - seatIds
+
+    missingSeatIds.singleOrNull { !missingSeatIds.contains(it + 1) && !missingSeatIds.contains(it - 1) }
+        .also { println("part 2: $it") }
 }
