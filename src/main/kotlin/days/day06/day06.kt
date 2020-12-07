1eddb8519a8158e6c031a3cada3e8e19d8721e1f
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-12-07 21:44:24 +0000

    Day 6: rewrite as union + intersection

--- src/main/kotlin/days/day06/day06.kt
+++ src/main/kotlin/days/day06/day06.kt
@@ -1,27 +1,41 @@
-package days
+package days.day06
 
 import lib.Reader
 
 fun main() {
-    val input = Reader("day06.txt").string()
+    val input = Reader("day06.txt").string().split("\n\n")
 
-    val part1 = input
-        .split("\n\n")
-        .map {
-            it.replace("\n", "").toSet().size
-        }.sum()
+    val part1 = input.sumBy { it.replace("\n", "").toSet().size }
 
     println(part1)
 
     val part2 = input
-            .split("\n\n")
             .map { it.split("\n") }
-            .map { group ->
+            .sumBy { group ->
                 ('a'..'z').count { letter ->
                     group.all { it.contains(letter) }
                 }
-            }.sum()
-
+            }
 
     println(part2)
+
+    val part1again = input
+        .sumBy {
+            it.split("\n")
+                .map(String::toSet)
+                .reduce { acc, list -> acc.union(list) }
+                .size
+        }
+
+    println(part1again)
+
+    val part2again = input
+        .sumBy {
+            it.split("\n")
+                .map(String::toSet)
+                .reduce { acc, list -> acc.intersect(list) }
+                .size
+        }
+
+    println(part2again)
 }
