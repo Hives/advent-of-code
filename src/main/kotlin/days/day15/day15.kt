d9862cfd6dab7c119a7870255be250a1978bcab8
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-12-15 09:25:04 +0000

    Day 15: part 2

--- src/main/kotlin/days/day15/day15.kt
+++ src/main/kotlin/days/day15/day15.kt
@@ -1,53 +1,46 @@
 package days.day15
 
+import lib.time
+
 val puzzleInput = listOf(9, 6, 0, 10, 18, 2, 1)
 
 fun main() {
-    val part1Example = CountingGame(listOf(0, 3, 6))
-    part1Example.go(2020)
-    // should equal 436
-    part1Example.current.also { println(it) }
 
-    val part1 = CountingGame(puzzleInput)
-    part1.go(2020)
-    // should equal 1238
-    part1.current.also { println(it) }
+    time ("part 1") {
+        val part1 = CountingGame(puzzleInput)
+        part1.go(2020)
+        // should equal 1238
+        part1.current
+    }
+
+    time ("part 2", 1, 0) {
+        val part2 = CountingGame(puzzleInput)
+        part2.go(30000000)
+        // should equal 3745954
+        part2.current
+    }
+
 }
 
 class CountingGame(startingNumbers: List<Int>) {
-    var history = startingNumbers.toMutableList()
-    val lastTimesNumberWasCalled =
+    private var history = startingNumbers.toMutableList()
+    private val lastTimesNumberWasCalled =
         startingNumbers.mapIndexed { index, i -> i to Pair<Int?, Int?>(null, index) }.toMap().toMutableMap()
-    var pointer = startingNumbers.size - 1
+    private var pointer = startingNumbers.size - 1
 
     val current: Int
         get() = history.last()
-
-    init {
-        debug()
-    }
 
     fun go(n: Int) {
         while (pointer < n - 1) crunch()
     }
 
-    fun crunch() {
+    private fun crunch() {
         val nextNumber = differenceBetweenLastTwoTimes(current)
 
         pointer++
         history.add(nextNumber)
         lastTimesNumberWasCalled[nextNumber] = Pair(lastTimesNumberWasCalled[nextNumber]?.second, pointer)
-    }
-
-    fun debug() {
-        println("--")
-        println("current:")
-        println(current)
-        println("lastTimesNumberWasCalled:")
-        println(lastTimesNumberWasCalled)
-        println("pointer:")
-        println(pointer)
-        println("--")
     }
 
     private fun differenceBetweenLastTwoTimes(n: Int): Int {
