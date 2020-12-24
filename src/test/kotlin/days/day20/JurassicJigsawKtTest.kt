cffbd2f31372b876deab7b6a2b40ef17ededfe63
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-12-24 09:19:38 +0000

    Day 20: Fix broken tests

--- src/test/kotlin/days/day20/JurassicJigsawKtTest.kt
+++ src/test/kotlin/days/day20/JurassicJigsawKtTest.kt
@@ -12,11 +12,11 @@ internal class JurassicJigsawKtTest {
         @Test
         fun `correct match`() {
             val tile1 = OrientedTile(
-                parseTile("xx1\nxx2\nxx3", 1),
+                Tile.from("Tile 1:\nxx1\nxx2\nxx3"),
                 Orientation(false, 0)
             )
             val tile2 = OrientedTile(
-                parseTile("1yy\n2yy\n3yy", 2),
+                Tile.from("Tile 2:\n1yy\n2yy\n3yy"),
                 Orientation(false, 0)
             )
             assertThat(tile1.alignsWith(tile2, 1)).isTrue()
@@ -29,11 +29,11 @@ internal class JurassicJigsawKtTest {
         @Test
         fun `correct match when flipped`() {
             val tile1 = OrientedTile(
-                parseTile("123\nxxx\nxxx", 1),
+                Tile.from("Tile 1:\n123\nxxx\nxxx"),
                 Orientation(false, 0)
             )
             val tile2 = OrientedTile(
-                parseTile("yyy\nyyy\n321", 2),
+                Tile.from("Tile 2:\nyyy\nyyy\n321"),
                 Orientation(true, 0)
             )
             assertThat(tile1.alignsWith(tile2, 0)).isTrue()
@@ -46,11 +46,11 @@ internal class JurassicJigsawKtTest {
         @Test
         fun `correct match when flipped and rotated`() {
             val tile1 = OrientedTile(
-                parseTile("xxx\nxxx\n123", 1),
+                Tile.from("Tile 1:\nxxx\nxxx\n123"),
                 Orientation(false, 0)
             )
             val tile2 = OrientedTile(
-                parseTile("yyy\nyyy\n123", 2),
+                Tile.from("Tile 2:\nyyy\nyyy\n123"),
                 Orientation(true, 2)
             )
             assertThat(tile1.alignsWith(tile2, 2)).isTrue()
