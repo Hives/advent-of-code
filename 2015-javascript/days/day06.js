61d6c964554fb568ae272beed993201c48d9785e
Paul Martin <paul.y.martin@johnlewis.co.uk>
2021-01-10 20:22:58 +0000

    Update 2015 Day 6 to use Typescript

--- 2015-javascript/days/day06.js
+++ 2015-javascript/days/day06.js
@@ -1,23 +1,30 @@
-const { readStrings } = require('../lib/reader.js')
-
-const input = readStrings("day06.txt")
-
-const processInstruction = (instructionString, rules) => {
-    const inst = parseInstruction(instructionString)
-
-    for (let j = inst.start.y; j <= inst.end.y; j++) {
-        for (let i = inst.start.x; i <= inst.end.x; i++) {
-            rules[inst.operation](i, j)
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+var readStrings = require('../lib/reader.js').readStrings;
+var Operation;
+(function (Operation) {
+    Operation["turn on"] = "TurnOn";
+    Operation["turn off"] = "TurnOff";
+    Operation["toggle"] = "Toggle";
+})(Operation || (Operation = {}));
+var input = readStrings("day06.txt");
+function processInstruction(instructionString, rules) {
+    var inst = parseInstruction(instructionString);
+    for (var y = inst.start.y; y <= inst.end.y; y++) {
+        for (var x = inst.start.x; x <= inst.end.x; x++) {
+            rules[inst.operation]({ x: x, y: y });
         }
     }
 }
-
-const parseInstruction = string => {
-    const [ , operation, x1, y1, x2, y2] =
-        string.match(/^(.*) (\d+),(\d+) through (\d+),(\d+)$/)
-
+function parseInstruction(instruction) {
+    var _a = instruction.match(/^(.*) (\d+),(\d+) through (\d+),(\d+)$/), operation = _a[1], x1 = _a[2], y1 = _a[3], x2 = _a[4], y2 = _a[5];
     return {
-        operation,
+        operation: Operation[operation],
         start: {
             x: parseInt(x1),
             y: parseInt(y1)
@@ -26,46 +33,40 @@ const parseInstruction = string => {
             x: parseInt(x2),
             y: parseInt(y2)
         }
-    }
+    };
 }
-
-const rules1 = {
-    "turn on": (x, y) => grid[y][x] = 1,
-    "turn off": (x, y) => grid[y][x] = 0,
-    "toggle": (x, y) => grid[y][x] = (grid[y][x] + 1) % 2 
+function sumArray(array) {
+    return array.reduce(function (acc, n) { return acc + n; });
 }
-
-const rules2 = {
-    "turn on": (x, y) => grid[y][x] = grid[y][x] + 1,
-    "turn off": (x, y) => grid[y][x] = Math.max(grid[y][x] - 1, 0),
-    "toggle": (x, y) => grid[y][x] = grid[y][x] + 2,
+function sumGrid() {
+    return sumArray(grid.flat());
 }
-
-const sumArray = array => array.reduce((acc, n) => acc + n)
-const sumGrid = () => sumArray(grid.flat())
-
-const testInstructions = [
+var testInstructions = [
     "turn on 0,0 through 7,7",
     "turn off 3,5 through 7,7",
     "toggle 1,1 through 6,6",
     "toggle 4,3 through 5,5"
-]
-
-const exampleInstructions = [
-    "turn on 0,0 through 999,999", 
+];
+var exampleInstructions = [
+    "turn on 0,0 through 999,999",
     "toggle 0,0 through 999,0",
     "turn off 499,499 through 500,500"
-]
-
-let grid
-
-const gridSize = 1000
-
-grid = Array(gridSize).map(_ => Array(gridSize).fill(0))
-input.forEach(i => processInstruction(i, rules1))
-console.log(`${sumGrid()} - should be 543903`)
-
-grid = Array(gridSize).map(_ => Array(gridSize).fill(0))
-input.forEach(i => processInstruction(i, rules2))
-console.log(`${sumGrid()} - should be 14687245`)
-
+];
+var rules1 = {
+    TurnOn: function (pnt) { return grid[pnt.y][pnt.x] = 1; },
+    TurnOff: function (pnt) { return grid[pnt.y][pnt.x] = 0; },
+    Toggle: function (pnt) { return grid[pnt.y][pnt.x] = (grid[pnt.y][pnt.x] + 1) % 2; }
+};
+var rules2 = {
+    TurnOn: function (pnt) { return grid[pnt.y][pnt.x] = grid[pnt.y][pnt.x] + 1; },
+    TurnOff: function (pnt) { return grid[pnt.y][pnt.x] = Math.max(grid[pnt.y][pnt.x] - 1, 0); },
+    Toggle: function (pnt) { return grid[pnt.y][pnt.x] = grid[pnt.y][pnt.x] + 2; }
+};
+var grid;
+var gridSize = 1000;
+grid = __spreadArrays(Array(gridSize)).map(function (_) { return Array(gridSize).fill(0); });
+input.forEach(function (i) { return processInstruction(i, rules1); });
+console.log(sumGrid() + " - should be 543903");
+grid = __spreadArrays(Array(gridSize)).map(function (_) { return Array(gridSize).fill(0); });
+input.forEach(function (i) { return processInstruction(i, rules2); });
+console.log(sumGrid() + " - should be 14687245");
---
+++ 2015-javascript/days/day06.ts
@@ -0,0 +1,102 @@
+const { readStrings } = require('../lib/reader.js')
+
+interface Point {
+    x: number,
+    y: number
+}
+
+enum Operation {
+    "turn on" = "TurnOn",
+    "turn off" = "TurnOff",
+    "toggle" = "Toggle"
+}
+
+type Rule = (p: Point) => void
+
+interface Rules {
+    TurnOn: Rule,
+    TurnOff: Rule,
+    Toggle: Rule
+}
+
+interface Instruction {
+    operation: Operation,
+    start: Point,
+    end: Point
+}
+
+const input: string[] = readStrings("day06.txt")
+
+function processInstruction(instructionString: string, rules: Rules): void {
+    const inst = parseInstruction(instructionString)
+    
+    for (let y = inst.start.y; y <= inst.end.y; y++) {
+        for (let x = inst.start.x; x <= inst.end.x; x++) {
+            rules[inst.operation]({x, y})
+        }
+    }
+}
+
+function parseInstruction(instruction: string): Instruction {
+    const [ , operation, x1, y1, x2, y2] =
+        instruction.match(/^(.*) (\d+),(\d+) through (\d+),(\d+)$/)
+
+    return {
+        operation: Operation[operation],
+        start: {
+            x: parseInt(x1),
+            y: parseInt(y1)
+        },
+        end: {
+            x: parseInt(x2),
+            y: parseInt(y2)
+        }
+    }
+}
+
+function sumArray(array: number[]): number {
+    return array.reduce((acc, n) => acc + n)
+}
+
+function sumGrid(): number {
+    return sumArray(grid.flat())
+}
+
+const testInstructions = [
+    "turn on 0,0 through 7,7",
+    "turn off 3,5 through 7,7",
+    "toggle 1,1 through 6,6",
+    "toggle 4,3 through 5,5"
+]
+
+const exampleInstructions = [
+    "turn on 0,0 through 999,999", 
+    "toggle 0,0 through 999,0",
+    "turn off 499,499 through 500,500"
+]
+
+const rules1: Rules = {
+    TurnOn: pnt => grid[pnt.y][pnt.x] = 1,
+    TurnOff: pnt => grid[pnt.y][pnt.x] = 0,
+    Toggle: pnt => grid[pnt.y][pnt.x] = (grid[pnt.y][pnt.x] + 1) % 2 
+}
+
+const rules2: Rules = {
+    TurnOn: pnt => grid[pnt.y][pnt.x] = grid[pnt.y][pnt.x] + 1,
+    TurnOff: pnt => grid[pnt.y][pnt.x] = Math.max(grid[pnt.y][pnt.x] - 1, 0),
+    Toggle: pnt => grid[pnt.y][pnt.x] = grid[pnt.y][pnt.x] + 2,
+}
+
+
+let grid: number[][]
+
+const gridSize = 1000
+
+grid = [...Array(gridSize)].map(_ => Array(gridSize).fill(0))
+input.forEach(i => processInstruction(i, rules1))
+console.log(`${sumGrid()} - should be 543903`)
+
+grid = [...Array(gridSize)].map(_ => Array(gridSize).fill(0))
+input.forEach(i => processInstruction(i, rules2))
+console.log(`${sumGrid()} - should be 14687245`)
+
