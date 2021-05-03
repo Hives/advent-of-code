eabef7dd91835c795f6c99d0fd55aebc5bcfc7ee
Paul Martin <paul.y.martin@johnlewis.co.uk>
2021-05-03 23:37:54 +0100

    2016 day 17 pt 2

--- aoc-2016-clojure/src/aoc_2016_clojure/day17.clj
+++ aoc-2016-clojure/src/aoc_2016_clojure/day17.clj
@@ -27,10 +27,10 @@
 
 (defn remove-walls [{:keys [x y]} directions]
   (->> directions
-       (remove-value-if (= min-x x) \L)
-       (remove-value-if (= max-x x) \R)
-       (remove-value-if (= min-y y) \U)
-       (remove-value-if (= max-y y) \D)))
+       (remove-value-if (= 0 x) \L)
+       (remove-value-if (= 3 x) \R)
+       (remove-value-if (= 0 y) \U)
+       (remove-value-if (= 3 y) \D)))
 
 (defn move [location direction]
   (let [unit-vectors            {\U {:x 0 :y -1}
@@ -41,7 +41,10 @@
         {old-x :x old-y :y}     location]
     {:x (+ old-x delta-x) :y (+ old-y delta-y)}))
 
-(defn foo [seed {:keys [path location]}]
+(defn is-complete? [{:keys [location]}]
+  (and (= (location :x) 3) (= (location :y) 3)))
+
+(defn generate-next-steps [seed {:keys [path location]}]
   (let [seed-plus-path (apply str (concat seed path))
         hash           (md5 seed-plus-path)
         next-directions (->> hash
@@ -52,15 +55,38 @@
             :location (move location direction)})
          next-directions)))
 
-(bar example-input)
-(bar puzzle-input)
-
-(defn bar [seed]
+(defn part-1 [seed]
   (loop [state [{:path [] :location {:x 0 :y 0}}]]
     (let [_ (println state)
-          complete-paths (filter #(= {:x 3 :y 3} (% :location)) state)]
+          complete-paths (filter is-complete? state)]
       (if (empty? state)
         ":("
         (if-not (empty? complete-paths)
-         complete-paths
-         (recur (mapcat #(foo seed %) state)))))))
+          complete-paths
+          (recur (mapcat #(generate-next-steps seed %) state)))))))
+
+;; (part-1 example-input)
+;; (part-1 puzzle-input)
+
+(defn part-2-one-step [seed state longest-path-length length]
+  (let [next-steps       (mapcat #(generate-next-steps seed %) state)
+        complete-paths   (filter is-complete? next-steps)
+        new-longest-path (if (empty? complete-paths)
+                           longest-path-length
+                           length)
+        new-state       (remove is-complete? next-steps)]
+    [new-state new-longest-path (inc length)]))
+
+(defn part-2 [seed]
+  (loop [state               [{:path [] :location {:x 0 :y 0}}]
+         longest-path-length 0
+         length              0]
+    (if (empty? state)
+      longest-path-length
+      (let [_                (println (str "paths: " (count state) " length: " length " longest-path-length: " longest-path-length))
+            [new-state new-longest-path new-length] (part-2-one-step seed state longest-path-length length)]
+        (recur new-state new-longest-path new-length)))))
+
+;; (part-2 "ihgpwlah")
+
+;; (part-2-one-step "ihgpwlah" [{:path [\D \D \R \R \R] :location {:x 3 :y 2}}] 0 5)
