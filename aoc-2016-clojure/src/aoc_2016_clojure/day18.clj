9b93aea91841da254e89a690a6459a171fd94ca8
Paul Martin <paul.y.martin@johnlewis.co.uk>
2021-05-09 19:24:30 +0100

    2016 day 18 better functional solution
    algorithm taken from a haskell solution on reddit

--- aoc-2016-clojure/src/aoc_2016_clojure/day18.clj
+++ aoc-2016-clojure/src/aoc_2016_clojure/day18.clj
@@ -4,40 +4,21 @@
 (def example-input ".^^.^.^^^^")
 
 (defn is-trap? [trio]
-  (let [trio-string (apply str trio)]
-    (or
-     (= "^^." trio-string)
-     (= ".^^" trio-string)
-     (= "^.." trio-string)
-     (= "..^" trio-string))))
+  (not (= (first trio) (last trio))))
 
 (defn get-next-row [row]
   (->> (concat [\.] row [\.])
        (partition 3 1)
        (map #(if (is-trap? %) \^ \.))))
 
-(defn get-rows-1 [first n]
-  (loop [rows [first]]
-    (if (= (count rows) n)
-      rows
-      (recur (conj rows (get-next-row (last rows)))))))
+(defn do-it [first n]
+  (->> first
+       (seq)
+       (iterate get-next-row)
+       (take n)
+       (flatten)
+       (filter #(= \. %))
+       (count)))
 
-(defn part-1 [input n]
-  (let [first (seq input)
-        final (map #(apply str %) (get-rows first n))
-        _     (println (clojure.string/join "\n" final))]
-    (count (remove #(= \^ %) (seq (clojure.string/join "" final))))))
-
-(defn get-rows-2 [first n]
-  (loop [row              first
-         safe-tiles-count 0
-         row-count        1]
-    (let [new-safe-tiles-count (+
-                                safe-tiles-count
-                                (count (filter #(= \. %) row)))]
-      (if (= n row-count)
-        new-safe-tiles-count
-        (recur (get-next-row row) new-safe-tiles-count (inc row-count))))))
-
-;; (part-1 puzzle-input 40)
-;; (get-rows-2 puzzle-input 400000) ;; slow (~1m)
+(do-it puzzle-input 40)
+;; (do-it puzzle-input 400000) ;; slow >1m
