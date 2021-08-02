4111e56ee35078d5f9b354a1b5b89510c701ea6d
Paul Martin <paul.y.martin@johnlewis.co.uk>
2021-08-02 14:02:14 +0100

    2017 day 2

---
+++ aoc-2017-python/day_02/day_02.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python3
+
+from functools import reduce
+from itertools import permutations
+
+puzzle_input = [[int(n) for n in line.strip().split("\t")] for line in open("input.txt").readlines()]
+
+
+def part_1(spreadsheet):
+    return reduce(
+        lambda acc, row: acc + max(row) - min(row),
+        spreadsheet,
+        0
+    )
+
+
+def find_even_division(row):
+    pair_which_evenly_divides = next(filter(
+        lambda p: p[0] % p[1] == 0,
+        list(permutations(row, 2))
+    ))
+
+    return pair_which_evenly_divides[0] // pair_which_evenly_divides[1]
+
+
+def part_2(spreadsheet):
+    return reduce(
+        lambda acc, row: acc + find_even_division(row),
+        spreadsheet,
+        0
+    )
+
+
+print(part_1(puzzle_input))
+print(part_2(puzzle_input))
