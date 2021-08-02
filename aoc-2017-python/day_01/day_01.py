6311f26560af5e8390d460b3df3cc4172ba9bd92
Paul Martin <paul.y.martin@johnlewis.co.uk>
2021-08-02 09:41:03 +0100

    2017 day 1 - some refactoring

--- aoc-2017-python/day_01/day_01.py
+++ aoc-2017-python/day_01/day_01.py
@@ -6,37 +6,30 @@ puzzle_input = [int(char) for char in open("input.txt").read().strip()]
 
 
 def pair_with_next(xs):
-    return list(map(
-        lambda n: [xs[n], xs[(n + 1) % len(xs)]],
-        range(len(xs))))
+    return [[xs[n], xs[(n + 1) % len(xs)]] for n in range(len(xs))]
 
 
 def pair_with_opposite(xs):
-    return list(map(
-        lambda n: [xs[n], xs[(n + (len(xs) // 2)) % len(xs)]],
-        range(len(xs))))
+    return [[xs[n], xs[(n + (len(xs) // 2)) % len(xs)]] for n in range(len(xs))]
 
 
 def take_value_if_equal(pair):
-    if pair[0] == pair[1]:
-        return pair[0]
-    else:
-        return 0
+    return pair[0] if pair[0] == pair[1] else 0
 
 
-def solve_captcha(input, make_pairs):
+def solve_captcha(numbers, make_pairs):
     return reduce(
         lambda acc, pair: acc + take_value_if_equal(pair),
-        make_pairs(input),
+        make_pairs(numbers),
         0)
 
 
-def part_1(input):
-    return solve_captcha(input, pair_with_next)
+def part_1(captcha):
+    return solve_captcha(captcha, pair_with_next)
 
 
-def part_2(input):
-    return solve_captcha(input, pair_with_opposite)
+def part_2(captcha):
+    return solve_captcha(captcha, pair_with_opposite)
 
 
 print(part_1(puzzle_input))
