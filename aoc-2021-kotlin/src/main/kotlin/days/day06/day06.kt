ccfd7f8b1d3989569a310013f8127a421d4df4a7
Paul Martin <paul.y.martin@johnlewis.co.uk>
2021-12-06 09:18:37 +0000

    2021 day 6 refactor

--- aoc-2021-kotlin/src/main/kotlin/days/day06/day06.kt
+++ aoc-2021-kotlin/src/main/kotlin/days/day06/day06.kt
@@ -7,10 +7,12 @@ import lib.time
 fun main() {
     val input = Reader("day06.txt").string()
     val exampleInput = Reader("day06-example.txt").string()
-    time {
+
+    time(iterations = 10_000, warmUpIterations = 10_000, message = "Part 1") {
         part1(input)
     }.checkAnswer(356190L)
-    time {
+
+    time(iterations = 10_000, warmUpIterations = 10_000, message = "Part 2") {
         part2(input)
     }.checkAnswer(1617359101538L)
 }
@@ -18,39 +20,26 @@ fun main() {
 fun part1(input: String) = runGenerations(parseInput(input), 80)
 fun part2(input: String) = runGenerations(parseInput(input), 256)
 
-fun runGenerations(initialFish: Map<Int, Long>, requiredDays: Int): Long {
-    fun go(fish: Map<Int, Long>, days: Int): Long {
-        return if (days == 0) fish.values.sum()
+fun runGenerations(initialAges: List<Long>, requiredDays: Int): Long {
+    tailrec fun go(ages: List<Long>, days: Int): Long =
+        if (days == 0) ages.sum()
         else {
-            go(mapOf(
-                0 to fish[1]!!,
-                1 to fish[2]!!,
-                2 to fish[3]!!,
-                3 to fish[4]!!,
-                4 to fish[5]!!,
-                5 to fish[6]!!,
-                6 to fish[7]!! + fish[0]!!,
-                7 to fish[8]!!,
-                8 to fish[0]!!,
-            ), days - 1)
+            val newAges = ages.rotateLeft().toMutableList()
+            newAges[6] += ages[0]
+            go(newAges.toList(), days - 1)
         }
-    }
-    return go(initialFish, requiredDays)
+    return go(initialAges, requiredDays)
 }
+
+fun <T> List<T>.rotateLeft() = this.subList(1, this.size) + this[0]
 
 fun parseInput(input: String) =
     input.split(",").map { it.toInt() }
-        .groupBy { it }
-        .let {
-            mapOf(
-                0 to (it[0]?.size ?: 0).toLong(),
-                1 to (it[1]?.size ?: 0).toLong(),
-                2 to (it[2]?.size ?: 0).toLong(),
-                3 to (it[3]?.size ?: 0).toLong(),
-                4 to (it[4]?.size ?: 0).toLong(),
-                5 to (it[5]?.size ?: 0).toLong(),
-                6 to (it[6]?.size ?: 0).toLong(),
-                7 to (it[7]?.size ?: 0).toLong(),
-                8 to (it[8]?.size ?: 0).toLong(),
-            )
+        .groupingBy { it }.eachCount()
+        .let { agesCount ->
+            val agesList = MutableList(9) { 0L }
+            agesCount.forEach { (age, count) ->
+                agesList[age] = count.toLong()
+            }
+            agesList.toList()
         }
