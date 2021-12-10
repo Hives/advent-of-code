ed16052b83bfa50c2214672d7890d23850e5f068
Paul Martin <paul.y.martin@johnlewis.co.uk>
2021-12-10 09:30:10 +0000

    2021 day 10 refactor with sealed class

--- aoc-2021-kotlin/src/main/kotlin/days/day10/day10.kt
+++ aoc-2021-kotlin/src/main/kotlin/days/day10/day10.kt
@@ -1,97 +1,122 @@
 package days.day10
 
+import days.day10.ValidationStatus.Corrupted
+import days.day10.ValidationStatus.Incomplete
+import days.day10.ValidationStatus.OK
 import lib.Reader
+import lib.checkAnswer
+import lib.time
 
 fun main() {
     val input = Reader("day10.txt").strings()
     val exampleInput = Reader("day10-example.txt").strings()
 
-    input.map { validate(it.toList()) }
-        .filterIsInstance<ValidationStatus.Corrupted>()
-        .sumOf { score(it.char) }
-        .also { println(it) }
+    time(message = "Part 1") {
+        part1(input)
+    }.checkAnswer(367059)
 
-    input.map { validate(it.toList()) }
-        .filterIsInstance<ValidationStatus.Incomplete>()
-        .map { completeStack(it.stack).let(::scoreCompletion) }
-        .sorted()
-        .let { it[it.size/2] }
-        .also { println(it) }
+    time(message = "Part 2") {
+        part2(input)
+    }.checkAnswer(1952146692L)
 }
 
-fun validate(line: List<Char>): ValidationStatus {
-    tailrec fun go(openerStack: List<Char>, remaining: List<Char>): ValidationStatus {
+fun part1(input: List<String>) =
+    input.map { validate(parse(it)) }
+        .filterIsInstance<Corrupted>()
+        .sumOf { score(it.closer) }
+
+fun part2(input: List<String>) =
+    input.map { validate(parse(it)) }
+        .filterIsInstance<Incomplete>()
+        .map { scoreIncompleteStack(it.stack) }
+        .sorted()
+        .let { it[it.size / 2] }
+
+fun parse(input: String) = input.toList().map(Bracket::from)
+
+fun validate(line: List<Bracket>): ValidationStatus {
+    tailrec fun go(stack: List<Bracket.Opener>, remaining: List<Bracket>): ValidationStatus {
         if (remaining.isEmpty()) {
-            return if (openerStack.isEmpty()) ValidationStatus.OK
-            else ValidationStatus.Incomplete(openerStack)
+            return if (stack.isEmpty()) OK
+            else Incomplete(stack)
         }
 
-        val next = remaining.first()
-        when (next) {
-            '(' -> return go(openerStack + next, remaining.drop(1))
-            '[' -> return go(openerStack + next, remaining.drop(1))
-            '{' -> return go(openerStack + next, remaining.drop(1))
-            '<' -> return go(openerStack + next, remaining.drop(1))
-            ')' -> {
-                return if (openerStack.last() == '(') go(openerStack.subList(0, (openerStack.size - 1)),
-                    remaining.drop(1))
-                else ValidationStatus.Corrupted(next)
+        return when (val next = remaining.first()) {
+            is Bracket.Opener -> go(stack + next, remaining.drop(1))
+            is Bracket.Closer -> {
+                if (stack.last() == next.opener) go(stack.dropLast(1), remaining.drop(1))
+                else Corrupted(next)
             }
-            ']' -> {
-                return if (openerStack.last() == '[') go(openerStack.subList(0, (openerStack.size - 1)),
-                    remaining.drop(1))
-                else ValidationStatus.Corrupted(next)
-            }
-            '}' -> {
-                return if (openerStack.last() == '{') go(openerStack.subList(0, (openerStack.size - 1)),
-                    remaining.drop(1))
-                else ValidationStatus.Corrupted(next)
-            }
-            '>' -> {
-                return if (openerStack.last() == '<') go(openerStack.subList(0, (openerStack.size - 1)),
-                    remaining.drop(1))
-                else ValidationStatus.Corrupted(next)
-            }
-            else -> throw Exception("Unrecognised character?!?! $next")
         }
     }
 
     return go(emptyList(), line)
 }
 
-fun completeStack(openerStack: List<Char>) =
-    openerStack.reversed().fold(emptyList<Char>()) { acc, char ->
-        when (char) {
-            '(' -> acc + ')'
-            '[' -> acc + ']'
-            '{' -> acc + '}'
-            '<' -> acc + '>'
-            else -> throw Exception("Unrecognised character?!?! $char")
+fun score(closer: Bracket.Closer) =
+    when (closer) {
+        Bracket.Closer.Paren -> 3
+        Bracket.Closer.Square -> 57
+        Bracket.Closer.Curly -> 1197
+        Bracket.Closer.Pointy -> 25137
+    }
+
+fun scoreIncompleteStack(stack: List<Bracket.Opener>) =
+    stack.reversed().fold(0L) { score, opener ->
+        when (opener) {
+            Bracket.Opener.Paren -> (score * 5) + 1
+            Bracket.Opener.Square -> (score * 5) + 2
+            Bracket.Opener.Curly -> (score * 5) + 3
+            Bracket.Opener.Pointy -> (score * 5) + 4
         }
     }
 
-fun score(char: Char) =
-    when (char) {
-        ')' -> 3
-        ']' -> 57
-        '}' -> 1197
-        '>' -> 25137
-        else -> throw Exception("Unrecognised character?!?! $char")
+sealed class Bracket {
+    sealed class Opener : Bracket() {
+        object Paren : Opener()
+        object Square : Opener()
+        object Curly : Opener()
+        object Pointy : Opener()
     }
 
-fun scoreCompletion(chars: List<Char>) =
-    chars.fold(0L) { score, char ->
-        when (char) {
-            ')' -> (score * 5) + 1L
-            ']' -> (score * 5) + 2L
-            '}' -> (score * 5) + 3L
-            '>' -> (score * 5) + 4L
-            else -> throw Exception("Unrecognised character?!?! $char")
+    sealed class Closer : Bracket() {
+        abstract val opener: Opener
+
+        object Paren : Closer() {
+            override val opener = Opener.Paren
+        }
+
+        object Square : Closer() {
+            override val opener = Opener.Square
+        }
+
+        object Curly : Closer() {
+            override val opener = Opener.Curly
+        }
+
+        object Pointy : Closer() {
+            override val opener = Opener.Pointy
         }
     }
+
+    companion object {
+        fun from(char: Char): Bracket =
+            when (char) {
+                '(' -> Opener.Paren
+                '[' -> Opener.Square
+                '{' -> Opener.Curly
+                '<' -> Opener.Pointy
+                ')' -> Closer.Paren
+                ']' -> Closer.Square
+                '}' -> Closer.Curly
+                '>' -> Closer.Pointy
+                else -> throw Exception("Unrecognised character?!?! $char")
+            }
+    }
+}
 
 sealed class ValidationStatus {
     object OK : ValidationStatus()
-    data class Incomplete(val stack: List<Char>) : ValidationStatus()
-    data class Corrupted(val char: Char) : ValidationStatus()
+    data class Incomplete(val stack: List<Bracket.Opener>) : ValidationStatus()
+    data class Corrupted(val closer: Bracket.Closer) : ValidationStatus()
 }
