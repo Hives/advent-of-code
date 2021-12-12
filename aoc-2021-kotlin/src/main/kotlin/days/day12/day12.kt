eaefe02b835298a1824af681e6c48e7006e0c4fb
Paul Martin <paul.y.martin@johnlewis.co.uk>
2021-12-12 12:59:52 +0000

    2021 day 12 refactor

--- aoc-2021-kotlin/src/main/kotlin/days/day12/day12.kt
+++ aoc-2021-kotlin/src/main/kotlin/days/day12/day12.kt
@@ -1,34 +1,33 @@
 package days.day12
 
 import lib.Reader
+import lib.checkAnswer
+import lib.time
 
 fun main() {
     val input = Reader("day12.txt").strings()
-    val exampleInput1 = Reader("day12-example1.txt").strings()
+    val exampleInput = Reader("day12-example.txt").strings()
 
-    part1(input).also { println(it) }
-    part2(input).also { println(it) }
+    time(message = "Part 1") {
+        part1(input)
+    }.checkAnswer(3887)
+
+    time(iterations = 5, warmUpIterations = 0, message = "Part 2") {
+        part2(input)
+    }.checkAnswer(104834)
 }
 
-fun part1(input: List<String>): Int {
+fun part1(input: List<String>): Int = countPaths(input, ::pathValidator1)
+fun part2(input: List<String>): Int = countPaths(input, ::pathValidator2)
+
+fun countPaths(
+    input: List<String>,
+    pathValidator: (List<String>, String) -> Boolean,
+): Int {
     val caveConnections = parseInput(input)
 
     fun go(journeys: List<List<String>>): List<List<String>> {
-        val extensions = tick(journeys, caveConnections)
-
-        return if (extensions.size == journeys.size) journeys
-        else go(extensions)
-    }
-
-    val allJourneys = go(listOf(listOf("start")))
-    return allJourneys.size
-}
-
-fun part2(input: List<String>): Int {
-    val caveConnections = parseInput(input)
-
-    fun go(journeys: List<List<String>>): List<List<String>> {
-        val extensions = tick2(journeys, caveConnections)
+        val extensions = tick(journeys, caveConnections, pathValidator)
 
         return if (extensions.size == journeys.size) journeys
         else go(extensions)
@@ -42,43 +41,38 @@ fun part2(input: List<String>): Int {
 fun tick(
     journeys: List<List<String>>,
     caveConnections: Map<String, List<String>>,
-): List<List<String>> = journeys.flatMap { journey ->
-    if (journey.last() == "end") listOf(journey)
-    else {
-        extendPath(journey, caveConnections)
+    pathValidator: (List<String>, String) -> Boolean,
+): List<List<String>> =
+    journeys.flatMap { journey ->
+        if (journey.last() == "end") listOf(journey)
+        else {
+            extendPath(journey, caveConnections, pathValidator)
+        }
     }
-}
-
-fun tick2(
-    journeys: List<List<String>>,
-    caveConnections: Map<String, List<String>>,
-): List<List<String>> = journeys.flatMap { journey ->
-    if (journey.last() == "end") listOf(journey)
-    else {
-        extendPath2(journey, caveConnections)
-    }
-}
 
 fun extendPath(
     journey: List<String>,
     caveConnections: Map<String, List<String>>,
+    pathValidator: (List<String>, String) -> Boolean,
 ): List<List<String>> =
     caveConnections[journey.last()]?.mapNotNull { next ->
-        if (next.isLowercase() && next in journey) null
-        else journey + next
+        if (pathValidator(journey, next)) journey + next
+        else null
     } ?: throw Exception("Unknown cave: ${journey.last()}")
 
-fun extendPath2(
-    journey: List<String>,
-    caveConnections: Map<String, List<String>>,
-): List<List<String>> =
-    caveConnections[journey.last()]?.mapNotNull { next ->
-        if (
-            next == "start" ||
-            next.isLowercase() && next in journey && journey.containsDuplicateLowercase()
-        ) null
-        else journey + next
-    } ?: throw Exception("Unknown cave: ${journey.last()}")
+fun pathValidator1(journey: List<String>, next: String) =
+    when {
+        next == "start" -> false
+        next.isLowercase() && next in journey -> false
+        else -> true
+    }
+
+fun pathValidator2(journey: List<String>, next: String) =
+    when {
+        next == "start" -> false
+        next.isLowercase() && next in journey && journey.containsDuplicateLowercase() -> false
+        else -> true
+    }
 
 fun List<String>.containsDuplicateLowercase() =
     this.filter { it.isLowercase() }
