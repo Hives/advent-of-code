7b015dbc10bcf9d368223c018bf99e947c309c90
Paul Martin <paul.y.martin@johnlewis.co.uk>
2021-12-12 11:34:33 +0000

    2021 day 12 part 2

--- aoc-2021-kotlin/src/main/kotlin/days/day12/day12.kt
+++ aoc-2021-kotlin/src/main/kotlin/days/day12/day12.kt
@@ -7,6 +7,7 @@ fun main() {
     val exampleInput1 = Reader("day12-example1.txt").strings()
 
     part1(input).also { println(it) }
+    part2(input).also { println(it) }
 }
 
 fun part1(input: List<String>): Int {
@@ -23,6 +24,21 @@ fun part1(input: List<String>): Int {
     return allJourneys.size
 }
 
+fun part2(input: List<String>): Int {
+    val caveConnections = parseInput(input)
+
+    fun go(journeys: List<List<String>>): List<List<String>> {
+        val extensions = tick2(journeys, caveConnections)
+
+        return if (extensions.size == journeys.size) journeys
+        else go(extensions)
+    }
+
+    val allJourneys = go(listOf(listOf("start")))
+
+    return allJourneys.size
+}
+
 fun tick(
     journeys: List<List<String>>,
     caveConnections: Map<String, List<String>>,
@@ -30,6 +46,16 @@ fun tick(
     if (journey.last() == "end") listOf(journey)
     else {
         extendPath(journey, caveConnections)
+    }
+}
+
+fun tick2(
+    journeys: List<List<String>>,
+    caveConnections: Map<String, List<String>>,
+): List<List<String>> = journeys.flatMap { journey ->
+    if (journey.last() == "end") listOf(journey)
+    else {
+        extendPath2(journey, caveConnections)
     }
 }
 
@@ -41,6 +67,24 @@ fun extendPath(
         if (next.isLowercase() && next in journey) null
         else journey + next
     } ?: throw Exception("Unknown cave: ${journey.last()}")
+
+fun extendPath2(
+    journey: List<String>,
+    caveConnections: Map<String, List<String>>,
+): List<List<String>> =
+    caveConnections[journey.last()]?.mapNotNull { next ->
+        if (
+            next == "start" ||
+            next.isLowercase() && next in journey && journey.containsDuplicateLowercase()
+        ) null
+        else journey + next
+    } ?: throw Exception("Unknown cave: ${journey.last()}")
+
+fun List<String>.containsDuplicateLowercase() =
+    this.filter { it.isLowercase() }
+        .groupingBy { it }
+        .eachCount()
+        .any { (_, value) -> value > 1 }
 
 fun String.isLowercase() = this == this.lowercase()
 
