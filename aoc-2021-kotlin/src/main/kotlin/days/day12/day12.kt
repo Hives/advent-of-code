3e760e61930ecff47052512cbc00087a8ab41e91
Paul Martin <paul.y.martin@johnlewis.co.uk>
2021-12-12 11:17:35 +0000

    2021 day 12 part 1

---
+++ aoc-2021-kotlin/src/main/kotlin/days/day12/day12.kt
@@ -0,0 +1,52 @@
+package days.day12
+
+import lib.Reader
+
+fun main() {
+    val input = Reader("day12.txt").strings()
+    val exampleInput1 = Reader("day12-example1.txt").strings()
+
+    part1(input).also { println(it) }
+}
+
+fun part1(input: List<String>): Int {
+    val caveConnections = parseInput(input)
+
+    fun go(journeys: List<List<String>>): List<List<String>> {
+        val extensions = tick(journeys, caveConnections)
+
+        return if (extensions.size == journeys.size) journeys
+        else go(extensions)
+    }
+
+    val allJourneys = go(listOf(listOf("start")))
+    return allJourneys.size
+}
+
+fun tick(
+    journeys: List<List<String>>,
+    caveConnections: Map<String, List<String>>,
+): List<List<String>> = journeys.flatMap { journey ->
+    if (journey.last() == "end") listOf(journey)
+    else {
+        extendPath(journey, caveConnections)
+    }
+}
+
+fun extendPath(
+    journey: List<String>,
+    caveConnections: Map<String, List<String>>,
+): List<List<String>> =
+    caveConnections[journey.last()]?.mapNotNull { next ->
+        if (next.isLowercase() && next in journey) null
+        else journey + next
+    } ?: throw Exception("Unknown cave: ${journey.last()}")
+
+fun String.isLowercase() = this == this.lowercase()
+
+fun parseInput(input: List<String>) =
+    input
+        .map { it.split("-") }
+        .flatMap { listOf(it[0] to it[1], it[1] to it[0]) }
+        .groupBy { it.first }
+        .mapValues { (_, value) -> value.map { it.second } }
