0cb41ba223214e492193f0756bb07df493c3bcce
Paul Martin <paul.y.martin@johnlewis.co.uk>
2021-12-15 11:17:54 +0000

    2021 day 15 some refactoring

--- aoc-2021-kotlin/src/main/kotlin/days/day15/day15.kt
+++ aoc-2021-kotlin/src/main/kotlin/days/day15/day15.kt
@@ -13,17 +13,76 @@ fun main() {
         part1(input)
     }.checkAnswer(696)
 
-    time(message = "Part 2", iterations = 10, warmUpIterations = 10) {
+    time(message = "Part 2", iterations = 3, warmUpIterations = 0) {
         part2(input)
     }.checkAnswer(2952)
 }
 
 fun part1(input: List<String>) = solve(RiskLevels(parse(input)))
+fun part2(input: List<String>) = solve(RiskLevels(expandGrid(parse(input))))
 
-fun part2(input: List<String>): Int {
-    val topLeft = parse(input)
+fun solve(riskLevels: RiskLevels): Int {
+    val maxX = riskLevels.values.first().lastIndex
+    val maxY = riskLevels.values.lastIndex
 
-    val top = topLeft.map { row ->
+    val start = Vector(0, 0)
+    val end = Vector(maxX, maxY)
+
+    val queue = PriorityQueue<Vector>()
+    queue.put(start, 0)
+    val cameFrom = mutableMapOf<Vector, Vector?>()
+    val riskSoFar = mutableMapOf<Vector, Int>()
+    cameFrom[start] = null
+    riskSoFar[start] = 0
+
+    while (queue.isNotEmpty()) {
+        val current = queue.get()!!
+
+        if (current == end) break
+
+        current.neighboursInBounds(maxX, maxY)
+            .forEach { next ->
+                val newRisk = riskSoFar[current]!! + riskLevels.at(next)
+                if (next !in riskSoFar || newRisk < riskSoFar[next]!!) {
+                    riskSoFar[next] = newRisk
+                    queue.put(next, newRisk)
+                    cameFrom[next] = current
+                }
+            }
+    }
+
+    return riskSoFar[end]!!
+}
+
+fun Vector.neighboursInBounds(maxX: Int, maxY: Int) =
+    neighbours
+        .filterNot { it.x < 0 || it.x > maxX }
+        .filterNot { it.y < 0 || it.y > maxY }
+
+class PriorityQueue<T> private constructor(private val queue: MutableMap<T, Int>) {
+    fun put(value: T, priority: Int) {
+        queue[value] = priority
+    }
+
+    fun get(): T? {
+        val next = queue.minByOrNull { it.value }
+        if (next != null) queue.remove(next.key)
+        return next?.key
+    }
+
+    fun isNotEmpty() = queue.isNotEmpty()
+
+    companion object {
+        operator fun <S> invoke() = PriorityQueue(mutableMapOf<S, Int>())
+    }
+}
+
+data class RiskLevels(val values: List<List<Int>>) {
+    fun at(point: Vector) = values[point.y][point.x]
+}
+
+fun expandGrid(grid: List<List<Int>>): List<List<Int>> {
+    val top = grid.map { row ->
         val one = row
         val two = incRow(one)
         val three = incRow(two)
@@ -38,69 +97,10 @@ fun part2(input: List<String>): Int {
     val four = incGrid(three)
     val five = incGrid(four)
 
-    val bigGrid = one + two + three + four + five
-
-    val riskLevels = RiskLevels(bigGrid)
-    return solve(riskLevels)
+    return one + two + three + four + five
 }
 
-fun solve(riskLevels: RiskLevels): Int {
-    val start = Vector(0, 0)
-    val end = Vector(riskLevels.maxX, riskLevels.maxY)
-
-    val queue = PriorityQueue(mutableMapOf<Vector, Int>())
-    queue.put(start, 0)
-    val cameFrom = mutableMapOf<Vector, Vector?>()
-    val riskSoFar = mutableMapOf<Vector, Int>()
-    cameFrom[start] = null
-    riskSoFar[start] = 0
-
-    while (queue.isNotEmpty()) {
-        val current = queue.get()!!
-
-        if (current == end) break
-
-        current.neighbours
-            .filterNot { it.x < 0 || it.x > riskLevels.maxX }
-            .filterNot { it.y < 0 || it.y > riskLevels.maxY }
-            .forEach { next ->
-                val newRisk = riskSoFar[current]!! + riskLevels.at(next)
-                if (next !in riskSoFar.keys || newRisk < riskSoFar[next]!!) {
-                    riskSoFar[next] = newRisk
-                    queue.put(next, newRisk)
-                    cameFrom[next] = current
-                }
-            }
-    }
-
-    return riskSoFar[end]!!
-}
-
-data class PriorityQueue<T, P : Comparable<P>>(private var queue: MutableMap<T, P>) {
-    fun put(value: T, priority: P) {
-        queue[value] = priority
-    }
-
-    fun get(): T? {
-        val item = queue.toList().minByOrNull { it.second }
-        item?.also { queue.remove(it.first) }
-        return item?.first
-    }
-
-    fun isNotEmpty() = queue.isNotEmpty()
-}
-
-data class RiskLevels(private val riskLevels: List<List<Int>>) {
-    fun at(point: Vector) = riskLevels[point.y][point.x]
-
-    val maxX = riskLevels.last().lastIndex
-    val maxY = riskLevels.lastIndex
-}
-
-fun incRow(row: List<Int>) = row.map {
-    val foo = it + 1
-    if (foo > 9) 1 else foo
-}
+fun incRow(row: List<Int>) = row.map { it % 9 + 1 }
 fun incGrid(grid: List<List<Int>>) = grid.map { row -> incRow(row) }
 
 fun parse(input: List<String>): List<List<Int>> =
