00f4c573375e11ec0d7ea5568b8e116a71400b0e
Paul Martin <paul.y.martin@johnlewis.co.uk>
2021-12-21 10:15:41 +0000

    2021 update timer

--- aoc-2021-kotlin/src/main/kotlin/lib/Timer.kt
+++ aoc-2021-kotlin/src/main/kotlin/lib/Timer.kt
@@ -8,9 +8,11 @@ fun time(iterations: Int = 100, warmUpIterations: Int = 15, message: String? = n
     println()
     if (!message.isNullOrEmpty()) println(message)
 
+    var answer = null as Any?
+
     repeat(warmUpIterations) { f() }
 
-    List(iterations) { measureNanoTime { f() } }
+    List(iterations) { measureNanoTime { answer = f() } }
         .also { times ->
             val average = times.average()
 
@@ -23,7 +25,7 @@ fun time(iterations: Int = 100, warmUpIterations: Int = 15, message: String? = n
             println("average time over $iterations iterations = $time")
         }
 
-    return f()
+    return answer
 }
 
 private fun round(number: Double) = BigDecimal(number).setScale(2, HALF_DOWN)
