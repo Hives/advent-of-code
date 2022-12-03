83321e02656cd8b95b7e74f33e3b3fa1e07502dd
Paul Martin <paul.y.martin@johnlewis.co.uk>
2022-12-03 12:17:20 +0000

    2022 day 3 smol change

--- aoc-2022-kotlin/src/main/kotlin/days/day03/day03.kt
+++ aoc-2022-kotlin/src/main/kotlin/days/day03/day03.kt
@@ -3,24 +3,23 @@ package days.day03
 import lib.Reader
 import lib.checkAnswer
 import lib.time
-import kotlin.streams.toList
 
 fun main() {
     val input = Reader("day03.txt").strings()
     val exampleInput = Reader("day03-example.txt").strings()
 
-    time(message = "Part 1") {
+    time(message = "Part 1", warmUpIterations = 50, iterations = 500) {
         part1(input)
     }.checkAnswer(8515)
 
-    time(message = "Part 2") {
+    time(message = "Part 2", warmUpIterations = 50, iterations = 500) {
         part2(input)
     }.checkAnswer(2434)
 }
 
 fun part1(lines: List<String>): Int =
     lines.sumOf { line ->
-        line.chars().toList()
+        line.toList()
             .let {
                 val length = it.size
                 it.chunked(length / 2)
@@ -28,6 +27,7 @@ fun part1(lines: List<String>): Int =
                 first.intersect(second.toSet())
             }
             .single()
+            .code
             .let {
                 if (it >= 97) it - 96
                 else it - 38
@@ -37,11 +37,12 @@ fun part1(lines: List<String>): Int =
 fun part2(lines: List<String>): Int =
     lines.chunked(3) { group ->
         group
-            .map { it.chars().toList() }
+            .map { it.toList() }
             .let { (first, second, third) ->
                 first.intersect(second.toSet()).intersect(third.toSet())
             }
             .single()
+            .code
             .let {
                 if (it >= 97) it - 96
                 else it - 38
