df4ec683dc36b89567df472fd135ce25b4d50b18
Paul Martin <paul.y.martin@johnlewis.co.uk>
2022-12-05 10:11:33 +0000

    2022 day 5 refactor

--- aoc-2022-kotlin/src/main/kotlin/days/day05/day05.kt
+++ aoc-2022-kotlin/src/main/kotlin/days/day05/day05.kt
@@ -42,22 +42,20 @@ fun part2(input: String): String {
 fun parse(input: String): Pair<List<MutableList<Char>>, List<Move>> {
     val (configLines, movesLines) = input.split("\n\n")
 
-    val config = configLines.lines().dropLast(1).map {
-        val s = mutableListOf<Char>()
-        it.forEachIndexed { index, c ->
-            if (((index + 1) % 2 == 0) && ((index + 1) % 4 != 0)) s += c
+    val stackCount = configLines.last().toString().toInt()
+    val stacks = List(stackCount) { mutableListOf<Char>() }
+    configLines.lines().dropLast(1).reversed().forEach { row ->
+        row.forEachIndexed { index, c ->
+            if (c.isLetter()) stacks[(index - 1) / 4].add(c)
         }
-        s.toList()
     }
-        .flipNotSquare()
-        .map { it.reversed().dropLastWhile { c -> c == ' ' }.toMutableList() }
 
     val moves = movesLines.lines().mapNotNull {
         val match = Regex("""move (\d+) from (\d+) to (\d+)""").find(it)
         match?.destructured?.let { (a, b, c) -> Move(count = a.toInt(), from = b.toInt() - 1, to = c.toInt() - 1) }
     }
 
-    return Pair(config, moves)
+    return Pair(stacks, moves)
 }
 
 data class Move(val count: Int, val from: Int, val to: Int)
