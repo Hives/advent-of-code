27fa7b4c70697b8247e943196643267461de5538
Paul Martin <paul.y.martin@johnlewis.co.uk>
2022-12-22 15:41:33 +0000

    2022 day 22 part 2

--- aoc-2022-kotlin/src/main/kotlin/days/day22_part2/day22_part2.kt
+++ aoc-2022-kotlin/src/main/kotlin/days/day22_part2/day22_part2.kt
@@ -2,84 +2,341 @@ package days.day22_part2
 
 import days.day22_part1.Instruction
 import lib.Reader
-
-enum class InputType { EXAMPLE, REAL }
+import lib.Vector
+import lib.checkAnswer
 
 fun main() {
-    val input = Pair(Reader("day22.txt").string(), InputType.REAL)
-    val exampleInput = Pair(Reader("day22-example.txt").string(), InputType.EXAMPLE)
+    val input = Pair(Reader("day22.txt").string(), InputType.Real)
+    val exampleInput = Pair(Reader("day22-example.txt").string(), InputType.Example)
 
-    part2(exampleInput)
+    part2(exampleInput).checkAnswer(5031)
+    part2(input).checkAnswer(162038)
 }
 
-fun part2(input: Pair<String, InputType>) {
-    parse(input)
-}
+fun part2(input: Pair<String, InputType>): Int {
+    val (_, inputType) = input
+    val (map, instructions) = parse(input)
 
-typealias Grid = List<List<Char>>
-fun Grid.p() = forEach { println(it.joinToString("")) }
+    val initial = Vector(
+        x = map[0].indexOfFirst { it == '.' },
+        y = 0
+    )
 
-data class Cube(val front: Grid, val right: Grid, val back: Grid, val left: Grid, val top: Grid, val bottom: Grid) {
-    fun p() {
-        println("\ntop:")
-        top.p()
-        println("\nfront:")
-        front.p()
-        println("\nright:")
-        right.p()
-        println("\nback:")
-        back.p()
-        println("\nleft:")
-        left.p()
-        println("\nbottom:")
-        bottom.p()
+    val warpZonesHit = mutableSetOf<WarpZone>()
+    val path = mutableMapOf<Vector, Char>()
+
+    fun updatePath(place: Vector, facing: Vector?) {
+        path[place] = when (facing) {
+            null -> 'O'
+            Vector(0, 1) -> 'v'
+            Vector(0, -1) -> '^'
+            Vector(1, 0) -> '>'
+            Vector(-1, 0) -> '<'
+            else -> throw Exception("Bad facing: $facing")
+        }
     }
+
+    fun forward1(current: Vector, facing: Vector): Pair<Vector, Vector> {
+        updatePath(current, facing)
+
+        val target = current + facing
+
+        val (nextTarget, nextFacing) = when (map.at(target)) {
+            '.' -> Pair(target, facing)
+            '#' -> Pair(current, facing)
+            else -> {
+                val warpZone = inputType.warpZones.single { target in it.points && facing == it.inFacing }
+                warpZonesHit.add(warpZone)
+                val warpedTarget = warpZone.pointTransform(target)
+                val next = when (map.at(warpedTarget)) {
+                    '.' -> Pair(warpedTarget, warpZone.outFacing)
+                    '#' -> Pair(current, facing)
+                    else -> throw Exception("Couldn't find a place?")
+                }
+                next
+            }
+        }
+
+        return Pair(nextTarget, nextFacing)
+    }
+
+    tailrec fun forward(distance: Int, current: Vector, facing: Vector): Pair<Vector, Vector> {
+        return if (distance == 0) Pair(current, facing)
+        else {
+            val (nextLocation, nextFacing) = forward1(current, facing)
+            forward(distance - 1, nextLocation, nextFacing)
+        }
+    }
+
+    fun printy() {
+        map.mapIndexed { y, row ->
+            row.mapIndexed { x, cell ->
+                val v = Vector(x, y)
+                if (v in path) path[v]
+                else cell
+            }.joinToString("")
+        }.forEach(::println)
+    }
+
+
+    val (finalLocation, finalFacing) = instructions.fold(
+        Pair(
+            initial,
+            Unit.Right.v
+        )
+    ) { (current, facing), instruction ->
+        when (instruction) {
+            is Instruction.Forward -> forward(instruction.distance, current, facing)
+            is Instruction.Turn -> {
+                val newFacing = when (instruction) {
+                    Instruction.Turn.Left -> facing.rotateRight() // LOL
+                    Instruction.Turn.Right -> facing.rotateLeft() // LOL
+                }
+                Pair(current, newFacing)
+            }
+        }
+    }
+    updatePath(finalLocation, finalFacing)
+
+    printy()
+
+    val finalFacingScore = when (finalFacing) {
+        Vector(0, -1) -> 3
+        Vector(0, 1) -> 1
+        Vector(1, 0) -> 0
+        Vector(-1, 0) -> 2
+        else -> throw Exception("Bad final facing $finalFacing")
+    }
+    return ((finalLocation.y + 1) * 1000) + ((finalLocation.x + 1) * 4) + finalFacingScore
 }
+
+fun <T> List<List<T>>.at(v: Vector): T? = try {
+    this[v.y][v.x]
+} catch (e: Exception) {
+    null
+}
+
+sealed class InputType(val warpZones: List<WarpZone>) {
+    object Example : InputType(
+        listOf(
+            WarpZone(
+                id = 1,
+                points = (8..11).map { Vector(it, -1) }.toSet(),
+                pointTransform = { v -> Vector(11 - v.x, 4) },
+                inFacing = Unit.Up.v,
+                outFacing = Unit.Down.v
+            ),
+            WarpZone(
+                id = 2,
+                points = (0..3).map { Vector(12, it) }.toSet(),
+                pointTransform = { v -> Vector(15, 11 - v.y) },
+                inFacing = Unit.Right.v,
+                outFacing = Unit.Left.v,
+            ),
+            WarpZone(
+                id = 3,
+                points = (4..7).map { Vector(12, it) }.toSet(),
+                pointTransform = { v -> Vector(19 - v.y, 8) },
+                inFacing = Unit.Right.v,
+                outFacing = Unit.Down.v,
+            ),
+            WarpZone(
+                id = 4,
+                points = (12..15).map { Vector(it, 7) }.toSet(),
+                pointTransform = { v -> Vector(11, 19 - v.x) },
+                inFacing = Unit.Up.v,
+                outFacing = Unit.Left.v,
+            ),
+            WarpZone(
+                id = 5,
+                points = (8..11).map { Vector(16, it) }.toSet(),
+                pointTransform = { v -> Vector(11, 11 - v.y) },
+                inFacing = Unit.Right.v,
+                outFacing = Unit.Left.v,
+            ),
+            WarpZone(
+                id = 6,
+                points = (12..15).map { Vector(it, 12) }.toSet(),
+                pointTransform = { v -> Vector(0, 19 - v.x) },
+                inFacing = Unit.Down.v,
+                outFacing = Unit.Right.v,
+            ),
+            WarpZone(
+                id = 7,
+                points = (8..11).map { Vector(it, 12) }.toSet(),
+                pointTransform = { v -> Vector(11 - v.x, 7) },
+                inFacing = Unit.Down.v,
+                outFacing = Unit.Up.v,
+            ),
+            WarpZone(
+                id = 8,
+                points = (8..11).map { Vector(7, it) }.toSet(),
+                pointTransform = { v -> Vector(15 - v.y, 7) },
+                inFacing = Unit.Left.v,
+                outFacing = Unit.Up.v,
+            ),
+            WarpZone(
+                id = 9,
+                points = (4..7).map { Vector(it, 8) }.toSet(),
+                pointTransform = { v -> Vector(8, 15 - v.x) },
+                inFacing = Unit.Down.v,
+                outFacing = Unit.Right.v,
+            ),
+            WarpZone(
+                id = 10,
+                points = (0..3).map { Vector(it, 8) }.toSet(),
+                pointTransform = { v -> Vector(11 - v.x, 11) },
+                inFacing = Unit.Down.v,
+                outFacing = Unit.Up.v,
+            ),
+            WarpZone(
+                id = 11,
+                points = (4..7).map { Vector(-1, it) }.toSet(),
+                pointTransform = { v -> Vector(19 - v.y, 11) },
+                inFacing = Unit.Left.v,
+                outFacing = Unit.Up.v,
+            ),
+            WarpZone(
+                id = 12,
+                points = (0..3).map { Vector(it, 3) }.toSet(),
+                pointTransform = { v -> Vector(11 - v.x, 0) },
+                inFacing = Unit.Up.v,
+                outFacing = Unit.Down.v,
+            ),
+            WarpZone(
+                id = 13,
+                points = (4..7).map { Vector(it, 3) }.toSet(),
+                pointTransform = { v -> Vector(8, v.x - 4) },
+                inFacing = Unit.Up.v,
+                outFacing = Unit.Right.v,
+            ),
+            WarpZone(
+                id = 14,
+                points = (0..3).map { Vector(7, it) }.toSet(),
+                pointTransform = { v -> Vector(v.y + 4, 4) },
+                inFacing = Unit.Left.v,
+                outFacing = Unit.Down.v,
+            )
+        )
+    )
+
+    object Real : InputType(
+        listOf(
+            WarpZone(
+                id = 1,
+                points = (50..99).map { Vector(it, -1) }.toSet(),
+                pointTransform = { v -> Vector(0, v.x + 100) },
+                inFacing = Unit.Up.v,
+                outFacing = Unit.Right.v
+            ),
+            WarpZone(
+                id = 2,
+                points = (100..149).map { Vector(it, -1) }.toSet(),
+                pointTransform = { v -> Vector(v.x - 100, 199) },
+                inFacing = Unit.Up.v,
+                outFacing = Unit.Up.v
+            ),
+            WarpZone(
+                id = 3,
+                points = (0..49).map { Vector(150, it) }.toSet(),
+                pointTransform = { v -> Vector(99, 149 - v.y) },
+                inFacing = Unit.Right.v,
+                outFacing = Unit.Left.v
+            ),
+            WarpZone(
+                id = 4,
+                points = (100..149).map { Vector(it, 50) }.toSet(),
+                pointTransform = { v -> Vector(99, v.x - 50) },
+                inFacing = Unit.Down.v,
+                outFacing = Unit.Left.v
+            ),
+            WarpZone(
+                id = 5,
+                points = (50..99).map { Vector(100, it) }.toSet(),
+                pointTransform = { v -> Vector(v.y + 50, 49) },
+                inFacing = Unit.Right.v,
+                outFacing = Unit.Up.v
+            ),
+            WarpZone(
+                id = 6,
+                points = (100..149).map { Vector(100, it) }.toSet(),
+                pointTransform = { v -> Vector(149, 149 - v.y) },
+                inFacing = Unit.Right.v,
+                outFacing = Unit.Left.v
+            ),
+            WarpZone(
+                id = 7,
+                points = (50..99).map { Vector(it, 150) }.toSet(),
+                pointTransform = { v -> Vector(49, v.x + 100) },
+                inFacing = Unit.Down.v,
+                outFacing = Unit.Left.v
+            ),
+            WarpZone(
+                id = 8,
+                points = (150..199).map { Vector(50, it) }.toSet(),
+                pointTransform = { v -> Vector(v.y - 100, 149) },
+                inFacing = Unit.Right.v,
+                outFacing = Unit.Up.v
+            ),
+            WarpZone(
+                id = 9,
+                points = (0..49).map { Vector(it, 200) }.toSet(),
+                pointTransform = { v -> Vector(v.x + 100, 0) },
+                inFacing = Unit.Down.v,
+                outFacing = Unit.Down.v
+            ),
+            WarpZone(
+                id = 10,
+                points = (150..199).map { Vector(-1, it) }.toSet(),
+                pointTransform = { v -> Vector(v.y - 100, 0) },
+                inFacing = Unit.Left.v,
+                outFacing = Unit.Down.v
+            ),
+            WarpZone(
+                id = 11,
+                points = (100..149).map { Vector(-1, it) }.toSet(),
+                pointTransform = { v -> Vector(50, 149 - v.y) },
+                inFacing = Unit.Left.v,
+                outFacing = Unit.Right.v
+            ),
+            WarpZone(
+                id = 12,
+                points = (0..49).map { Vector(it, 99) }.toSet(),
+                pointTransform = { v -> Vector(50, v.x + 50) },
+                inFacing = Unit.Up.v,
+                outFacing = Unit.Right.v
+            ),
+            WarpZone(
+                id = 13,
+                points = (50..99).map { Vector(49, it) }.toSet(),
+                pointTransform = { v -> Vector(v.y - 50, 100) },
+                inFacing = Unit.Left.v,
+                outFacing = Unit.Down.v
+            ),
+            WarpZone(
+                id = 14,
+                points = (0..49).map { Vector(49, it) }.toSet(),
+                pointTransform = { v -> Vector(0, 149 - v.y) },
+                inFacing = Unit.Left.v,
+                outFacing = Unit.Right.v
+            ),
+        )
+    )
+}
+
+data class WarpZone(
+    val id: Int,
+    val points: Set<Vector>,
+    val pointTransform: (Vector) -> Vector,
+    val inFacing: Vector,
+    val outFacing: Vector
+)
 
 fun parse(inputAndType: Pair<String, InputType>): Pair<List<List<Char>>, List<Instruction>> {
     val (input, inputType) = inputAndType
-    val squareSize = when (inputType) {
-        InputType.EXAMPLE -> 4
-        InputType.REAL -> 50
-    }
 
     val (first, second) = input.split("\n\n")
     val map = first.lines().map { it.toList() }
-
-    val rows = map.chunked(squareSize)
-    val squares = rows.flatMap { rowOfSquares ->
-        val n = (rowOfSquares.first().size + 1) / squareSize
-        (0 until n).map { m ->
-            rowOfSquares.map { row ->
-                row.subList(m * squareSize, (m + 1) * squareSize)
-            }
-        }
-    }.filter { it.first().first() != ' ' }
-
-    val cube = when (inputType) {
-        InputType.EXAMPLE -> {
-            Cube(
-                front = squares[0],
-                right = squares[5].rotateCCW(1),
-                back = squares[4].rotateCCW(2),
-                left = squares[2].rotateCCW(3),
-                top = squares[1].rotateCCW(2),
-                bottom = squares[3],
-            )
-        }
-        InputType.REAL -> TODO()
-    }
-
-    cube.p()
-
-    squares.first().let {
-        fun List<List<Char>>.p() {
-            println()
-            println()
-            println()
-            forEach { println(it.joinToString("")) }
-        }
-    }
 
     val (instructions, remainder) = second.fold(Pair(emptyList<Instruction>(), emptyList<Char>())) { acc, nextChar ->
         val (instructions, numbers) = acc
@@ -108,3 +365,10 @@ fun <T> List<List<T>>.rotateCCW(): List<List<T>> =
             this[x][size - y - 1]
         }
     }
+
+sealed class Unit(val v: Vector) {
+    object Up : Unit(Vector(0, -1))
+    object Down : Unit(Vector(0, 1))
+    object Left : Unit(Vector(-1, 0))
+    object Right : Unit(Vector(1, 0))
+}
