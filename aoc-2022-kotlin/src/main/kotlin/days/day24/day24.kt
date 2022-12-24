5e00096daea13a41fb0e21e0bfcf1d4b12aee915
Paul Martin <paul.y.martin@johnlewis.co.uk>
2022-12-24 11:41:16 +0000

    2022 day 24 refactor

--- aoc-2022-kotlin/src/main/kotlin/days/day24/day24.kt
+++ aoc-2022-kotlin/src/main/kotlin/days/day24/day24.kt
@@ -9,7 +9,7 @@ fun main() {
     val exampleInput = Reader("day24-example.txt").strings()
     val simpleExampleInput = Reader("day24-simple-example.txt").strings()
 
-    part1(input)
+    part1(exampleInput)
 }
 
 fun part1(input: List<String>) {
@@ -18,6 +18,8 @@ fun part1(input: List<String>) {
     aStar(initial)
         .also { println(it.size) }
 }
+
+fun generateBoardPermutations(initial: State) {}
 
 fun reconstructPath(cameFrom: Map<State, State>, start: Vector, goal: State): List<State> {
     println("reconstructin")
@@ -32,7 +34,9 @@ fun reconstructPath(cameFrom: Map<State, State>, start: Vector, goal: State): Li
 }
 
 fun aStar(initial: State): List<State> {
-    val goal = initial.destination
+    val goal = initial.board.destination
+    val start = initial.board.initial
+
     fun h(state: State) = (goal - state.position).manhattanDistance
     val openSet2 = PriorityQueue(compareBy<State> { -h(it) })
     openSet2.add(initial)
@@ -44,7 +48,7 @@ fun aStar(initial: State): List<State> {
         val current = openSet2.poll()
 //        println(current.position)
         if (current.position == goal) {
-            return reconstructPath(cameFrom, initial.initial, current)
+            return reconstructPath(cameFrom, start, current)
         } else {
             current.possibleNextStates().forEach { neighbour ->
                 val tentativeGScore = gScore[current]!! + 1
@@ -63,52 +67,34 @@ fun aStar(initial: State): List<State> {
 }
 
 data class State(
-    val position: Vector,
-    val blizzards: Map<Vector, List<Vector>>,
-    val minX: Int,
-    val maxX: Int,
-    val minY: Int,
-    val maxY: Int,
+    val board: Board,
+    val position: Vector
 ) {
-    val initial = Vector(1, 0)
-    val destination = Vector(maxX - 1, maxY)
-
     fun possibleNextStates(): List<State> {
-        val newBlizzards = updateBlizzards()
+        val newBoard = board.updateBlizzards()
         val newPositions = Moves.values().map { position + it.v }.filter {
-            it !in newBlizzards.keys && isWithinBounds(it)
+            it !in newBoard.blizzards.keys && newBoard.isWithinBounds(it)
         }
-        return newPositions.map { this.copy(position = it, blizzards = newBlizzards) }
+        return newPositions.map {
+            this.copy(position = it, board = newBoard)
+        }
     }
-
-    fun isWithinBounds(position: Vector): Boolean =
-        position == destination || position == initial || (
-                (position.x in (minX + 1) until maxX) &&
-                        (position.y in (minY + 1) until maxY)
-                )
-
-    fun updateBlizzards() =
-        this.blizzards.flatMap { (location, directions) ->
-            directions.map { direction ->
-                updateOneBlizzard(location, direction) to direction
-            }
-        }.groupBy({ it.first }) { it.second }
 
     fun printy() {
         println()
-        (minY..maxY).map { y ->
+        (board.minY..board.maxY).map { y ->
             when (y) {
-                minY -> List(maxX - minX + 1) { '#' }.joinToString("")
-                maxY -> List(maxX - minX + 1) { '#' }.joinToString("")
-                else -> (minX..maxX).joinToString("") { x ->
+                board.minY -> List(board.maxX - board.minX + 1) { '#' }.joinToString("")
+                board.maxY -> List(board.maxX - board.minX + 1) { '#' }.joinToString("")
+                else -> (board.minX..board.maxX).joinToString("") { x ->
                     val v = Vector(x, y)
                     when {
                         v == position -> "E"
-                        x == minX -> "#"
-                        x == maxX -> "#"
-                        v in blizzards.keys -> {
-                            if (blizzards[v]!!.size > 1) blizzards[v]!!.size.toString()
-                            else when (blizzards[v]!!.single()) {
+                        x == board.minX -> "#"
+                        x == board.maxX -> "#"
+                        v in board.blizzards.keys -> {
+                            if (board.blizzards[v]!!.size > 1) board.blizzards[v]!!.size.toString()
+                            else when (board.blizzards[v]!!.single()) {
                                 Moves.UP.v -> "^"
                                 Moves.DOWN.v -> "v"
                                 Moves.LEFT.v -> "<"
@@ -123,6 +109,33 @@ data class State(
             }
         }.forEach { println(it) }
     }
+
+}
+
+data class Board(
+    val blizzards: Map<Vector, List<Vector>>,
+    val minX: Int,
+    val maxX: Int,
+    val minY: Int,
+    val maxY: Int,
+) {
+    val initial = Vector(1, 0)
+    val destination = Vector(maxX - 1, maxY)
+
+    fun updateBlizzards(): Board {
+        val newBlizzards = this.blizzards.flatMap { (location, directions) ->
+            directions.map { direction ->
+                updateOneBlizzard(location, direction) to direction
+            }
+        }.groupBy({ it.first }) { it.second }
+        return copy(blizzards = newBlizzards)
+    }
+
+    fun isWithinBounds(position: Vector): Boolean =
+        position == destination || position == initial || (
+                (position.x in (minX + 1) until maxX) &&
+                        (position.y in (minY + 1) until maxY)
+                )
 
     private fun updateOneBlizzard(location: Vector, direction: Vector): Vector {
         val moved = location + direction
@@ -158,11 +171,13 @@ fun parse(input: List<String>): State {
 
     return State(
         position = Vector(1, 0),
-        blizzards = blizzards,
-        minX = 0,
-        maxX = input.first().length - 1,
-        minY = 0,
-        maxY = input.size - 1,
+        board = Board(
+            blizzards = blizzards,
+            minX = 0,
+            maxX = input.first().length - 1,
+            minY = 0,
+            maxY = input.size - 1,
+        )
     )
 }
 
