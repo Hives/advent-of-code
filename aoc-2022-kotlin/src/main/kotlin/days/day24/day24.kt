7f3fddf91834d407243c6f556141fb801ea625a8
Paul Martin <paul.y.martin@johnlewis.co.uk>
2022-12-25 02:32:20 +0000

    2022 day 24 refactor

--- aoc-2022-kotlin/src/main/kotlin/days/day24/day24.kt
+++ aoc-2022-kotlin/src/main/kotlin/days/day24/day24.kt
@@ -3,10 +3,13 @@ package days.day24
 import lib.Reader
 import lib.Vector
 import java.util.PriorityQueue
+import kotlin.system.exitProcess
 
 fun main() {
     val input = Reader("day24.txt").strings()
+    // not 251
     // not 252
+    // not 253
     // not 279
     // 285 is too high
     // 337 is too high (also 336)
@@ -18,13 +21,14 @@ fun main() {
 }
 
 fun part1(input: List<String>) {
-    val initialBoard = parse(input)
-    val boardPermutations = generateBoardPermutations(initialBoard)
-    val start = initialBoard.initial
-    val goal = initialBoard.destination
+    val board = parse(input)
+    val start = board.start
+    val goal = board.goal
 
-    val initialState = State(position = start, boardIndex = 0)
-    val path = aStar(initialState, boardPermutations)
+    val initialState = State(position = start, time = 0)
+    val path = aStar(initialState, board)
+
+    println(path.map { it.position })
     val stepValidation = path.windowed(2, 1).all {
         val from = it[0]
         val to = it[1]
@@ -34,32 +38,64 @@ fun part1(input: List<String>) {
     println("start validation: ${path.first().position == start}")
     println("end validation: ${path.last().position == goal}")
     val blizzardValidation = path.all {
-        it.position !in boardPermutations[it.boardIndex].blizzards
+        it.position !in board.getBlizzard(it.time)
     }
     println("blizzard validation: $blizzardValidation")
 
     println("time taken: ${path.size - 1}")
 }
 
-fun generateBoardPermutations(initialBoard: Board): List<Board> {
-    val boardPermutations = mutableListOf(initialBoard)
+fun aStar(initial: State, board: Board): List<State> {
+    fun heuristic(state: State) = (state.position - board.goal).manhattanDistance
 
-    tailrec fun go(board: Board) {
-        val newBoard = board.updateBlizzards()
-        if (newBoard == boardPermutations.first()) return
-        else {
-            boardPermutations.add(newBoard)
-            go(newBoard)
+    val cameFrom = mutableMapOf<State, State>()
+
+    val cheapestKnownCostsToNodes = mutableMapOf(initial to 0)
+
+    val estimatedCostToGoal = mutableMapOf(initial to heuristic(initial))
+
+    val openSet = PriorityQueue(compareBy<State> { estimatedCostToGoal[it] })
+    openSet.add(initial)
+
+    var counter = 0
+
+    while (openSet.isNotEmpty()) {
+        counter++;
+        if (counter == 100000) exitProcess(0)
+//        println()
+//        println("openSet:")
+//        openSet.forEach { println("$it - estimated cost to goal ${estimatedCostToGoal[it]}") }
+        val current = openSet.poll()
+
+//        println("current: $current - score: ${cheapestKnownCostsToNodes[current]}")
+//        board.printy(current.position, current.time)
+
+        if (current.position == board.goal) {
+            return reconstructPath(cameFrom, board.start, current)
+        } else {
+            current.possibleNextStates(board).also {
+//                println("possible next locations: ${it.map { it.position }}")
+            }.forEach { nextState ->
+//                println()
+//                println("nextState: $nextState")
+                val tentativeGScore = cheapestKnownCostsToNodes[current]!! + 1
+//                println("tentativeGScore: $tentativeGScore")
+//                println("cheapest known cost to that node: ${cheapestKnownCostsToNodes.getOrDefault(nextState, INFINITY)}")
+                if (tentativeGScore < cheapestKnownCostsToNodes.getOrDefault(nextState, INFINITY)) {
+//                    println("havin that")
+                    cameFrom[nextState] = current
+                    cheapestKnownCostsToNodes[nextState] = tentativeGScore
+                    estimatedCostToGoal[nextState] = tentativeGScore + heuristic(nextState)
+//                    println("estimated cost to goal: ${estimatedCostToGoal[nextState]}")
+                    openSet.add(nextState)
+                }
+            }
         }
     }
-
-    go(initialBoard)
-
-    return boardPermutations
+    throw Exception("open set is empty but goal was not reached")
 }
 
 fun reconstructPath(cameFrom: Map<State, State>, start: Vector, goal: State): List<State> {
-    println("reconstructin")
     val path = mutableListOf(goal)
     var current = goal
     while (current.position != start) {
@@ -70,71 +106,56 @@ fun reconstructPath(cameFrom: Map<State, State>, start: Vector, goal: State): Li
     return path.reversed()
 }
 
-fun aStar(initial: State, boards: List<Board>): List<State> {
-    val (start, goal) = boards.first().let { Pair(it.initial, it.destination) }
-    fun h(state: State) = (goal - state.position).manhattanDistance
-
-    val cameFrom = mutableMapOf<State, State>()
-
-    val gScore = mutableMapOf(initial to 0)
-
-    val fScore = mutableMapOf(initial to h(initial))
-
-    val openSet2 = PriorityQueue(compareBy<State> { fScore[it] })
-    openSet2.add(initial)
-
-    while (openSet2.isNotEmpty()) {
-        val current = openSet2.poll()
-//        println()
-//        println("current: $current - score: ${gScore[current]}")
-        if (current.position == goal) {
-            return reconstructPath(cameFrom, start, current)
-        } else {
-            current.possibleNextStates(boards).forEach { nextState ->
-//                println("nextState: $nextState")
-                val tentativeGScore = gScore[current]!! + 1
-//                println("tentativeGScore: $tentativeGScore")
-                if (tentativeGScore < gScore.getOrDefault(nextState, INFINITY)) {
-                    cameFrom[nextState] = current
-                    gScore[nextState] = tentativeGScore
-                    fScore[nextState] = tentativeGScore + h(nextState)
-                    openSet2.add(nextState)
-                }
-            }
-        }
-    }
-    throw Exception("open set is empty but goal was not reached")
-}
-
 data class State(
     val position: Vector,
-    val boardIndex: Int
+    val time: Int
 ) {
-    fun possibleNextStates(boards: List<Board>): List<State> {
-        val newBoardIndex = (boardIndex + 1) % boards.size
-        val newBoard = boards[newBoardIndex]
+    fun possibleNextStates(board: Board): List<State> {
+        val nextBlizzard = board.getBlizzard(time + 1)
         val newPositions = Moves.values().map { position + it.v }.filter {
-            it !in newBoard.blizzards.keys && newBoard.isWithinBounds(it)
+            it !in nextBlizzard.keys && board.isWithinBounds(it)
         }
         return newPositions.map { newPosition ->
-            State(position = newPosition, boardIndex = newBoardIndex)
+            State(
+                position = newPosition,
+                time = time + 1
+            )
         }
     }
 
-    fun printy(boards: List<Board>) {
+}
+
+typealias Blizzard = Map<Vector, List<Vector>>
+
+class Board(
+    initialBlizzard: Blizzard,
+    val minX: Int,
+    val maxX: Int,
+    val minY: Int,
+    val maxY: Int,
+) {
+    val start = Vector(1, 0)
+    val goal = Vector(maxX - 1, maxY)
+    private val blizzards = generateBoardPermutations(initialBlizzard)
+
+    fun getBlizzard(time: Int) = blizzards[time % blizzards.size]
+
+    fun printy(position: Vector, time: Int) {
         println()
-        val board = boards[boardIndex]
-        val grid = (0..board.maxY).map { y ->
-            (0..board.maxX).map { x ->
+        val blizzard = getBlizzard(time)
+        val grid = (0..maxY).map { y ->
+            (0..maxX).map { x ->
                 when {
-                    y == board.minY || y == board.maxY || x == board.minX || x == board.maxX -> "#"
                     Vector(x, y) == position -> "E"
+                    Vector(x, y) == start -> "."
+                    Vector(x, y) == goal -> "."
+                    y == minY || y == maxY || x == minX || x == maxX -> "#"
                     else -> "."
                 }
             }.toMutableList()
         }
 
-        board.blizzards.forEach { (location, directions) ->
+        blizzard.forEach { (location, directions) ->
             val c = if (directions.size > 1) "${directions.size}"
             else {
                 when (directions.single()) {
@@ -150,29 +171,34 @@ data class State(
 
         grid.forEach { println(it.joinToString("")) }
     }
-}
 
-data class Board(
-    val blizzards: Map<Vector, List<Vector>>,
-    val minX: Int,
-    val maxX: Int,
-    val minY: Int,
-    val maxY: Int,
-) {
-    val initial = Vector(1, 0)
-    val destination = Vector(maxX - 1, maxY)
+    private fun generateBoardPermutations(initialBlizzard: Blizzard): List<Blizzard> {
+        val blizzards = mutableListOf(initialBlizzard)
 
-    fun updateBlizzards(): Board {
-        val newBlizzards = this.blizzards.flatMap { (location, directions) ->
+        tailrec fun go(blizzard: Blizzard) {
+            val newBlizzard = nextBlizzard(blizzard)
+            if (newBlizzard == blizzards.first()) return
+            else {
+                blizzards.add(newBlizzard)
+                go(newBlizzard)
+            }
+        }
+
+        go(initialBlizzard)
+
+        return blizzards
+    }
+
+
+    private fun nextBlizzard(blizzard: Blizzard) =
+        blizzard.flatMap { (location, directions) ->
             directions.map { direction ->
                 updateOneBlizzard(location, direction) to direction
             }
         }.groupBy({ it.first }) { it.second }
-        return copy(blizzards = newBlizzards)
-    }
 
     fun isWithinBounds(position: Vector): Boolean =
-        position == destination || position == initial || (
+        position == goal || position == start || (
                 (position.x in (minX + 1) until maxX) &&
                         (position.y in (minY + 1) until maxY)
                 )
@@ -210,7 +236,7 @@ fun parse(input: List<String>): Board {
     }.toMap()
 
     return Board(
-        blizzards = blizzards,
+        initialBlizzard = blizzards,
         minX = 0,
         maxX = input.first().length - 1,
         minY = 0,
@@ -224,7 +250,7 @@ enum class Moves(val v: Vector) {
     RIGHT(Vector(1, 0)),
     DOWN(Vector(0, 1)),
     LEFT(Vector(-1, 0)),
-    WAIT(Vector(0, 0))
+    WAIT(Vector(0, 0)),
 }
 
 const val INFINITY = 1_000_000
