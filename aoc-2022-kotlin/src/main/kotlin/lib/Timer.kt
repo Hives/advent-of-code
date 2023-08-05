3670c1a7f4122126ff0b984cc2ee0ac516c2213d
Paul Martin <paul.y.martin@johnlewis.co.uk>
2023-08-05 12:05:35 +0100

    2022 update timer

--- aoc-2022-kotlin/src/main/kotlin/lib/Timer.kt
+++ aoc-2022-kotlin/src/main/kotlin/lib/Timer.kt
@@ -4,6 +4,10 @@ import java.math.BigDecimal
 import java.math.RoundingMode.HALF_DOWN
 import kotlin.system.measureNanoTime
 
+const val ONE_MINUTE = 60_000_000_000
+const val ONE_SECOND = 1_000_000_000
+const val ONE_MILLISECOND = 1_000_000
+
 fun <T> time(iterations: Int = 100, warmUpIterations: Int = 15, message: String? = null, f: () -> T?): T? {
     println()
     if (!message.isNullOrEmpty()) println(message)
@@ -17,12 +21,13 @@ fun <T> time(iterations: Int = 100, warmUpIterations: Int = 15, message: String?
             val average = times.average()
 
             val time = when {
-                average > 1_000_000_000 -> "${round(average / 1_000_000_000)} seconds"
-                average > 1_000_000 -> "${round(average / 1_000_000)} milliseconds"
+                average > ONE_MINUTE -> "${round(average / ONE_MINUTE)} minutes"
+                average > ONE_SECOND -> "${round(average / ONE_SECOND)} seconds"
+                average > ONE_MILLISECOND -> "${round(average / ONE_MILLISECOND)} milliseconds"
                 else -> "${round(average / 1_000)} microseconds"
             }
 
-            println("average time over $iterations iterations = $time")
+            println("average time over $iterations iteration(s) = $time")
         }
 
     return answer
