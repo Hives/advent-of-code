02ab23ff5048500914459135831b388d33b33258
Paul Martin <paul.y.martin@johnlewis.co.uk>
2023-08-06 22:01:55 +0100

    2019 day 1

---
+++ aoc-2019-go/day01/day01.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"reader"
+)
+
+func main() {
+	input := reader.Ints("./input.txt")
+	fmt.Println(part1(input))
+	fmt.Println(part2(input))
+	fmt.Println(getFuel2(1969))
+}
+
+func part1(masses []int) int {
+	sum := 0
+	for _, mass := range masses {
+		sum = sum + getFuel(mass)
+	}
+	return sum
+}
+
+func part2(masses []int) int {
+	sum := 0
+	for _, mass := range masses {
+		sum = sum + getFuel2(mass)
+	}
+	return sum
+}
+
+func getFuel(mass int) int {
+	return mass/3 - 2
+}
+
+func getFuel2(mass int) int {
+	fuel := getFuel(mass)
+	if fuel > 0 {
+		return fuel + getFuel2(fuel)
+	} else {
+		return 0
+	}
+}
---
+++ aoc-2019-go/reader/reader.go
@@ -0,0 +1,44 @@
+package reader
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+func String(path string) string {
+	return string(read(path))
+}
+
+func Strings(path string) (ret []string) {
+	ss := strings.Split(String(path), "\n")
+	for _, s := range ss {
+		if s != "" {
+			ret = append(ret, s)
+		}
+	}
+	return ret
+}
+
+func Ints(path string) []int {
+	input := Strings(path)
+	var ints []int
+	for _, s := range input {
+		n, err := strconv.Atoi(s)
+		check(err)
+		ints = append(ints, n)
+	}
+	return ints
+}
+
+func read(path string) []byte {
+	input, err := os.ReadFile(path)
+	check(err)
+	return input
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
