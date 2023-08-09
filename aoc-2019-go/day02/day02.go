49c9cef1d89f099ee512ce97d236c15840f9f65b
Paul Martin <paul.y.martin@johnlewis.co.uk>
2023-08-09 13:26:07 +0100

    2019 refactor day 2

--- aoc-2019-go/day02/day02.go
+++ aoc-2019-go/day02/day02.go
@@ -44,37 +44,72 @@ func part2(program []int) int {
 }
 
 func run(inputProgram []int, noun int, verb int) []int {
-	program := append([]int{}, inputProgram...)
-	program[1] = noun
-	program[2] = verb
+	state := state{
+		program: initialise(inputProgram, noun, verb),
+		pointer: 0,
+	}
 
-	pointer := 0
-
-	for program[pointer] != 99 {
-		opcode := program[pointer]
-		switch opcode {
+	for state.current() != 99 {
+		switch state.current() {
 		case 1:
-			// addition
-			input1Position := program[pointer+1]
-			input2Position := program[pointer+2]
-			outputPosition := program[pointer+3]
-
-			program[outputPosition] = program[input1Position] + program[input2Position]
-
-			pointer = pointer + 4
+			opcode1Addition(&state)
 		case 2:
-			// multiplication
-			input1Position := program[pointer+1]
-			input2Position := program[pointer+2]
-			outputPosition := program[pointer+3]
-
-			program[outputPosition] = program[input1Position] * program[input2Position]
-
-			pointer = pointer + 4
+			opcode2Multiplication(&state)
 		default:
-			panic(fmt.Sprintf("Unknown opcode: %v", opcode))
+			panic(fmt.Sprintf("Unknown opcode: %v", state.current()))
 		}
 	}
 
+	return state.program
+}
+
+func initialise(inputProgram []int, noun int, verb int) []int {
+	program := append([]int{}, inputProgram...)
+	program[1] = noun
+	program[2] = verb
 	return program
+}
+
+func opcode1Addition(s *state) {
+	input1Position := s.currentPlus(1)
+	input2Position := s.currentPlus(2)
+	outputPosition := s.currentPlus(3)
+
+	s.set(outputPosition, s.read(input1Position)+s.read(input2Position))
+	s.pointer = s.pointer + 4
+}
+
+func opcode2Multiplication(s *state) {
+	input1Position := s.currentPlus(1)
+	input2Position := s.currentPlus(2)
+	outputPosition := s.currentPlus(3)
+
+	s.set(outputPosition, s.read(input1Position)*s.read(input2Position))
+	s.pointer = s.pointer + 4
+}
+
+type state struct {
+	program []int
+	pointer int
+}
+
+func (s state) current() int {
+	return s.program[s.pointer]
+}
+
+func (s state) currentPlus(n int) int {
+	return s.program[s.pointer+n]
+}
+
+func (s state) read(i int) int {
+	return s.program[i]
+}
+
+func (s state) set(i int, value int) state {
+	program := s.program
+	program[i] = value
+	return state{
+		program: program,
+		pointer: s.pointer,
+	}
 }
