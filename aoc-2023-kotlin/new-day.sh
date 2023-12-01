1f8c69f6e4e7006adb26958f9bc5d7c36b9281ab
Paul Martin <paul.y.martin@johnlewis.co.uk>
2023-12-01 11:05:09 +0000

    2023: update new-day script

--- aoc-2023-kotlin/new-day.sh
+++ aoc-2023-kotlin/new-day.sh
@@ -1,10 +1,12 @@
 #! /usr/bin/env bash
 
-export DAY=$(date +"%d")
+day=$(date +"%d")
+day_no_leading_zeroes=$(echo $day | sed 's/^0*//')
+year=$(date +"%Y")
 
-path="src/main/kotlin/days/day${DAY}"
-input_path="src/main/resources"
-file="day${DAY}.kt"
+path="src/main/kotlin/days/day${day}"
+input_path="src/main/resources/day${day}"
+file="day${day}.kt"
 
 if [ -d $path ]; then
     echo "Directory ${path} already exists"
@@ -13,19 +15,27 @@ else
     mkdir -p $path
 fi;
 
+if [ -d $input_path ]; then
+    echo "Directory ${input_path} already exists"
+else
+    echo "Creating folder ${input_path}"
+    mkdir -p $input_path
+fi;
+
 echo "Downloading input"
 # requires COOKIE to be defined in .env
 source ./.env
-curl "https://adventofcode.com/2022/day/${DAY}/input" \
+curl "https://adventofcode.com/${year}/day/${day_no_leading_zeroes}/input" \
   -H "cookie: ${COOKIE}" \
-  -o "${input_path}/day${DAY}.txt"
+  -o "${input_path}/input.txt"
 
 echo "Creating empty example input file"
-touch "${input_path}/day${DAY}-example.txt"
+touch "${input_path}/example-1.txt"
 
 if [ -f "${path}/${file}" ]; then
     echo "File ${path}/${file} already exists"
 else
     echo "Creating file ${path}/${file}"
+    export DAY=$day
     envsubst < template.kt.txt > "${path}/${file}"
 fi;
--- aoc-2023-kotlin/src/main/kotlin/lib/Reader.kt
+++ aoc-2023-kotlin/src/main/kotlin/lib/Reader.kt
@@ -1,7 +1,7 @@
 package lib
 
 class Reader(private val file: String) {
-    fun string(): String = javaClass.getResource("/$file")!!.readText().trimEnd()
+    fun string(): String = javaClass.getResource("$file")!!.readText().trimEnd()
     fun strings(): List<String> = string().lines()
     fun chars(): List<Char> = string().toList()
     fun grid() = strings().map(String::toList)
