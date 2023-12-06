b62ebc7a214b5f7401b4c3fd1c3594acc0bd094a
Paul Martin <paul.y.martin@johnlewis.co.uk>
2023-12-06 09:44:18 +0000

    2023: day 6 part 2

--- aoc-2023-kotlin/src/main/kotlin/days/day06/day06.kt
+++ aoc-2023-kotlin/src/main/kotlin/days/day06/day06.kt
@@ -12,35 +12,45 @@ fun main() {
         part1(input)
     }.checkAnswer(500346)
 
-//    time(message = "Part 2") {
-//        part2(input)
-//    }.checkAnswer(0)
+    time(message = "Part 2", iterations = 1, warmUpIterations = 0) {
+        part2(input)
+    }.checkAnswer(42515755)
 }
 
 fun part1(input: List<String>): Int {
     val races = parse(input)
     return races.map { race ->
         (0..(race.time)).map { holdTime ->
-            race.calculateDistance(holdTime)
+            val travelTime = race.time - holdTime
+            travelTime * holdTime
         }.count { it > race.record }
     }.reduce(Int::times)
 }
 
 fun part2(input: List<String>): Int {
-    return -1
+    val race = parse2(input)
+    return (0..(race.time)).map { holdTime ->
+        val travelTime = race.time - holdTime
+        travelTime * holdTime
+    }.count { it > race.record }
 }
 
 fun parse(input: List<String>): List<Race> {
     fun parseLine(input: String) =
-        input.split(Regex(""":\s+"""))[1].split(Regex("""\s+""")).map(String::toInt)
+        input.split(Regex(""":\s+"""))[1].split(Regex("""\s+""")).map(String::toLong)
+
     val times = parseLine(input[0])
     val distances = parseLine(input[1])
     return times.zip(distances).map { Race(it.first, it.second) }
 }
 
-data class Race(val time: Int, val record: Int) {
-    fun calculateDistance(holdTime: Int): Int {
-        val travelTime = time - holdTime
-        return travelTime * holdTime
-    }
+fun parse2(input: List<String>): Race {
+    fun parseLine(input: String) =
+        input.split(Regex(""":\s+"""))[1].replace(Regex("""\s+"""), "").toLong()
+    return Race(
+        time = parseLine(input[0]),
+        record = parseLine(input[1])
+    )
 }
+
+data class Race(val time: Long, val record: Long)
