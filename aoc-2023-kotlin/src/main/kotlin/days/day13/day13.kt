537f642cebb8c103252eeb47bfac4c5e1477d73f
Paul Martin <paul.y.martin@johnlewis.co.uk>
2023-12-13 17:55:58 +0000

    2023: day 13

--- aoc-2023-kotlin/src/main/kotlin/days/day13/day13.kt
+++ aoc-2023-kotlin/src/main/kotlin/days/day13/day13.kt
@@ -2,9 +2,9 @@ package days.day13
 
 import days.day11.transpose
 import lib.Reader
+import lib.Vector
 import lib.checkAnswer
 import lib.time
-import kotlin.system.exitProcess
 
 fun main() {
     val input = Reader("/day13/input.txt").string()
@@ -14,25 +14,65 @@ fun main() {
         part1(input)
     }.checkAnswer(27664)
 
-    exitProcess(0)
-
     time(message = "Part 2") {
         part2(input)
-    }.checkAnswer(0)
+    }.checkAnswer(33991)
 }
 
 fun part1(input: String): Int =
-    parse(input).sumOf { grid ->
-        grid.findHorizontalReflectionLine()?.let { 100 * it }
-            ?: grid.transpose().findHorizontalReflectionLine()!!
-    }
+    parse(input).sumOf { it.summarize() }
 
-fun part2(input: String): Int {
-    return -1
+fun part2(input: String) =
+    parse(input).sumOf { it.summarizeAfterRemovingSmudge() }
+
+fun Grid.summarize(): Int {
+    val horizontal = findHorizontalReflectionLine()
+    val vertical = transpose().findHorizontalReflectionLine()
+    return if (horizontal != null) 100 * (horizontal + 1) else vertical!! + 1
 }
 
-fun Grid.findHorizontalReflectionLine(): Int? {
-    return (0..this.size - 2).firstOrNull { n ->
+fun Grid.summarizeAfterRemovingSmudge(): Int {
+    val horizontal = findNewHorizontalReflectionLineAfterRemovingSmudge()
+    val vertical = transpose().findNewHorizontalReflectionLineAfterRemovingSmudge()
+    return if (horizontal != null) 100 * (horizontal + 1) else vertical!! + 1
+}
+
+fun Grid.findNewHorizontalReflectionLineAfterRemovingSmudge(): Int? {
+    val existingHorizontalReflectionLine = findHorizontalReflectionLine()
+    val smudgeCandidates = findHorizontalSmudgeCandidates()
+    return smudgeCandidates.mapNotNull { smudge ->
+        val newGrid = removeSmudge(smudge)
+        val line = newGrid.findHorizontalReflectionLine(existingHorizontalReflectionLine)
+        if (line != null && line != existingHorizontalReflectionLine) {
+            line
+        } else {
+            null
+        }
+    }.also {
+        if (it.size > 1) throw Error("Oh no?!")
+    }.singleOrNull()
+}
+
+fun Grid.findHorizontalSmudgeCandidates(): Set<Vector> {
+    val candidates = mutableSetOf<Vector>()
+    indices.forEach { y1 ->
+        val row1 = this[y1]
+        ((y1 + 1) until size).forEach { y2 ->
+            val row2 = this[y2]
+            val differences = differInPlaces(row1, row2)
+            if (differences.size == 1) {
+                val x = differences.single()
+                if (row1[x] == '#') candidates.add(Vector(x, y1))
+                else candidates.add(Vector(x, y2))
+            }
+        }
+    }
+    return candidates
+}
+
+fun Grid.findHorizontalReflectionLine(butNotThisOne: Int? = null): Int? =
+    (0..this.size - 2).firstOrNull { n ->
+        if (n == butNotThisOne) return return@firstOrNull false
         var above = n
         var below = n + 1
         while (above >= 0 && below < size) {
@@ -43,8 +83,25 @@ fun Grid.findHorizontalReflectionLine(): Int? {
             below += 1
         }
         true
-    }?.let { it + 1 }
-}
+    }
+
+fun Grid.removeSmudge(smudge: Vector) =
+    mapIndexed { y, row ->
+        if (y == smudge.y) {
+            row.mapIndexed { x, char ->
+                if (x == smudge.x) '.' else char
+            }
+        } else {
+            row
+        }
+    }
+
+fun Grid.printy() = forEach { println(it.joinToString("")) }
+
+fun <T> differInPlaces(xs: List<T>, ys: List<T>): List<Int> =
+    xs.indices.mapNotNull { index ->
+        if (xs[index] != ys[index]) index else null
+    }
 
 fun parse(input: String): List<Grid> =
     input.split("\n\n").map { it.split("\n").map(String::toList) }
