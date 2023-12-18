6b13d9e310a5d435914b06caa8bfbd141548fbac
Paul Martin <paul.y.martin@johnlewis.co.uk>
2023-12-18 17:16:57 +0000

    2023: day 18 part 1

---
+++ aoc-2023-kotlin/src/main/kotlin/days/day18/part1/day18Part1.kt
@@ -0,0 +1,91 @@
+package days.day18.part1
+
+import days.day18.part1.Direction.*
+import lib.Reader
+import lib.Vector
+import lib.checkAnswer
+import lib.time
+
+fun main() {
+    val input = Reader("/day18/input.txt").strings()
+    val exampleInput = Reader("/day18/example-1.txt").strings()
+
+    time(message = "Part 1") {
+        part1(input)
+    }.checkAnswer(48795)
+}
+
+fun part1(input: List<String>): Int {
+    val path = createPath(input.map(::parse))
+    val insideStart = path.minBy { it.x } + Up + Right // 🤪
+    if (insideStart in path) throw Error("insidePath was part of path?!")
+    val inside = mutableSetOf(insideStart)
+    var front = setOf(insideStart)
+
+    while (front.isNotEmpty()) {
+        front = front.expand().filter { it !in path && it !in inside }.toSet()
+        inside.addAll(front)
+    }
+
+    return inside.count() + path.count()
+}
+
+fun createPath(instructions: List<Instruction>): Set<Vector> {
+    val path = mutableSetOf<Vector>()
+    var current = Vector(0, 0)
+    instructions.forEach { instruction ->
+        var n = 0
+        while (n < instruction.n) {
+            current += instruction.d
+            path.add(current)
+            n++
+        }
+    }
+    return path
+}
+
+fun parse(line: String): Instruction {
+    val match = Regex("""([LRUD]) (\d+)""").find(line)
+    val (d, n) = match!!.destructured
+    return Instruction(Direction.from(d), n.toInt())
+}
+
+fun Set<Vector>.printy() {
+    val minX = this.minOf { it.x }
+    val minY = this.minOf { it.y }
+    val maxX = this.maxOf { it.x }
+    val maxY = this.maxOf { it.y }
+    List(maxY - minY + 1) { y ->
+        List(maxX - minX + 1) { x ->
+            val v = Vector(x + minX, y + minY)
+            if (v in this) '#' else '.'
+        }.joinToString("").also(::println)
+    }
+}
+
+data class Instruction(val d: Direction, val n: Int)
+
+fun Set<Vector>.expand() = flatMap { point -> Direction.all.map { d -> point + d } }.toSet()
+
+operator fun Vector.plus(other: Direction) = Vector(
+    x = this.x + other.v.x,
+    y = this.y + other.v.y,
+)
+
+sealed class Direction(val v: Vector) {
+    object Left : Direction(Vector(-1, 0))
+    object Right : Direction(Vector(1, 0))
+    object Up : Direction(Vector(0, -1))
+    object Down : Direction(Vector(0, 1))
+    companion object {
+        fun from(s: String) = when (s) {
+            "L" -> Left
+            "R" -> Right
+            "U" -> Up
+            "D" -> Down
+            else -> throw Error("Oh no")
+        }
+
+        val all = listOf(Left, Right, Up, Down)
+    }
+}
