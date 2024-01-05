28c8fdf998c3c2c6b5fb6f36f3481367e1fa9f1e
Paul Martin <paul.y.martin@johnlewis.co.uk>
2024-01-05 00:00:29 +0000

    2023: day 24 part1 asdasd

--- aoc-2023-kotlin/src/main/kotlin/days/day24/day24.kt
+++ aoc-2023-kotlin/src/main/kotlin/days/day24/day24.kt
@@ -3,6 +3,8 @@ package days.day24
 import lib.Reader
 import lib.checkAnswer
 import lib.time
+import java.io.File
+import java.io.PrintWriter
 import kotlin.math.sign
 import kotlin.system.exitProcess
 
@@ -11,7 +13,10 @@ fun main() {
     val exampleInput = Reader("/day24/example-1.txt").strings()
 
     // 25308 is too high
-    println(part1(exampleInput))
+    // 25304 is too high
+    // 25269 is... not right
+    // 19968 is too low
+    println(part1(input))
 
     exitProcess(0)
 
@@ -27,39 +32,96 @@ fun main() {
 fun part1(input: List<String>): Int {
     val hailstones = input.map(::parse)
 
-    val exampleBoundaries = Pair(7L, 27L)
-    val realBoundaries = Pair(200000000000000, 400000000000000)
-
-    var count = 0
-
-    hailstones.indices.forEach { i1 ->
-        ((i1 + 1) until hailstones.size).forEach { i2 ->
-            val h1 = hailstones[i1]
-            val h2 = hailstones[i2]
-            val i = findIntersection(h1, h2)
-            if (i != null && isInFuture(i, h1) && isInFuture(i, h2)) {
-                if (isInsideBoundaries(i, exampleBoundaries)) count++
+    File("/home/hives/tmp/aoc-day-24-method-1.txt").printWriter().use { out ->
+        return hailstones.flatMapIndexed { index, h1 ->
+            hailstones.drop(index + 1).map { h2 ->
+                Pair(h1, h2)
             }
-        }
+        }.count { (h1, h2) -> crossInFuture(h1, h2, out) }
     }
 
-    return count
+}
+
+fun crossInFuture(h1: Hailstone, h2: Hailstone, out: PrintWriter): Boolean {
+    val i = findIntersection(h1, h2)
+    out.println("---")
+    out.println(h1)
+    out.println(h2)
+    return when {
+        i == null -> {
+            out.println("X: no cross")
+            false
+        }
+
+        !isInFuture(i, h1) && !isInFuture(i, h2) -> {
+            out.println("X: is in past for both: $i")
+            false
+        }
+
+        !isInFuture(i, h1) -> {
+            out.println("X: is in past for h1: $i")
+            false
+        }
+
+        !isInFuture(i, h2) -> {
+            out.println("X: is in past for h2: $i")
+            false
+        }
+
+        !isInsideBoundaries(i) -> {
+            out.println("X: outside boundaries: $i")
+            false
+        }
+
+        else -> {
+            out.println("Y: is good: $i")
+            true
+        }
+    }
 }
 
 fun part2(input: List<String>): Int {
     return -1
 }
 
-fun isInsideBoundaries(point: Pair<Double, Double>, boundaries: Pair<Long, Long>) =
-    point.first >= boundaries.first && point.second <= boundaries.second &&
+fun isInsideBoundaries(point: Pair<Double, Double>): Boolean {
+    val exampleBoundaries = Pair(7L, 27L)
+    val realBoundaries = Pair(200000000000000, 400000000000000)
+
+    val boundaries = realBoundaries
+
+    return point.first >= boundaries.first && point.second <= boundaries.second &&
             point.second >= boundaries.first && point.second <= boundaries.second
+}
 
 fun isInFuture(point: Pair<Double, Double>, hailstone: Hailstone): Boolean =
     sign(point.first - hailstone.pos.x) == sign(hailstone.vel.x.toDouble()) &&
             sign(point.second - hailstone.pos.y) == sign(hailstone.vel.y.toDouble())
 
+fun findIntersection2(h1: Hailstone, h2: Hailstone): Pair<Double, Double>? {
+    // from wikipedia, determinant method
+
+    val x1 = h1.pos.x.toDouble()
+    val y1 = h1.pos.y.toDouble()
+    val x2 = x1 + h1.vel.x
+    val y2 = y1 + h1.vel.y
+
+    val x3 = h2.pos.x.toDouble()
+    val y3 = h2.pos.y.toDouble()
+    val x4 = x3 + h2.vel.x
+    val y4 = y3 + h2.vel.y
+
+    val denominator = ((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4))
+    if (denominator == 0.toDouble()) return null
+
+    val numeratorX = (((x1 * y2) - (y1 * x2)) * (x3 - x4)) - ((x1 - x2) * ((x3 * y4) - (y3 * x4)))
+    val numeratorY = (((x1 * y2) - (y1 * x2)) * (y3 - y4)) - ((y1 - y2) * ((x3 * y4) - (y3 * x4)))
+
+    return Pair(numeratorX / denominator, numeratorY / denominator)
+}
+
 fun findIntersection(h1: Hailstone, h2: Hailstone): Pair<Double, Double>? {
-    if (areParallel(h1.vel, h2.vel)) return null
+    if (h1.vel.x == 0 || h2.vel.x == 0) throw Exception("was vertical?!")
 
     val (m1, c1) = getMAndC(h1)
     val (m2, c2) = getMAndC(h2)
@@ -76,8 +138,8 @@ fun findIntersection(h1: Hailstone, h2: Hailstone): Pair<Double, Double>? {
         }
 
         m1 == m2 -> {
-            // h2 || h1 - shouldn't happen
-            throw Exception("paths were parallel, but that shouldn't have happened: $h1 $h2")
+            // h2 || h1
+            return null
         }
 
         else -> {
@@ -89,26 +151,17 @@ fun findIntersection(h1: Hailstone, h2: Hailstone): Pair<Double, Double>? {
 }
 
 fun getMAndC(h: Hailstone): Pair<Double?, Double?> {
-    if (h.vel.x == 0) return Pair(null, null)
+//    if (h.vel.x == 0) return Pair(null, null)
     val m = h.vel.y.toDouble() / h.vel.x
     val c = h.pos.y - ((h.pos.x.toDouble() / h.vel.x) * h.vel.y)
     return Pair(m, c)
 }
 
-fun areParallel(v1: Velocity, v2: Velocity): Boolean {
-    val ratios = listOf(
-        v1.x.toDouble() / v2.x,
-        v1.y.toDouble() / v2.y,
-//        v1.z.toDouble() / v2.z,
-    )
-    return ratios.all { it == ratios[0] }
-}
-
 fun parse(line: String): Hailstone =
     line.split(" @ ").let { (p, v) ->
         Hailstone(
-            p.split(", ").let { (x, y, z) -> Position(x.trim().toLong(), y.trim().toLong(), z.trim().toLong()) },
-            v.split(", ").let { (x, y, z) -> Velocity(x.trim().toInt(), y.trim().toInt(), z.trim().toInt()) },
+            p.split(", ").let { (x, y, z) -> Position(x.toLong(), y.toLong(), z.toLong()) },
+            v.split(", ").let { (x, y, z) -> Velocity(x.toInt(), y.toInt(), z.toInt()) },
         )
     }
 
