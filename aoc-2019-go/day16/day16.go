d4bc2a70349cdd3d874839e597dc98900df78d86
Paul Martin <paul.y.martin@johnlewis.co.uk>
2024-11-23 13:48:32 +0000

    2019 day 16 part 2 wip

--- aoc-2019-go/day16/day16.go
+++ aoc-2019-go/day16/day16.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"aoc"
+	"fmt"
 	"reader"
 	"strconv"
 	"strings"
@@ -9,31 +10,117 @@ import (
 
 func main() {
 	input := reader.Digits("./input.txt")
-	//input := []int{1, 2, 3, 4, 5, 6, 7, 8}
-	//input := []int{8, 0, 8, 7, 1, 2, 2, 4, 5, 8, 5, 9, 1, 4, 5, 4, 6, 6, 1, 9, 0, 8, 3, 2, 1, 8, 6, 4, 5, 5, 9, 5}
-	aoc.CheckAnswer("Part 1", part1(input), "63483758")
+
+	//part1testInput1 := []int{8, 0, 8, 7, 1, 2, 2, 4, 5, 8, 5, 9, 1, 4, 5, 4, 6, 6, 1, 9, 0, 8, 3, 2, 1, 8, 6, 4, 5, 5, 9, 5}
+	//aoc.CheckAnswer("Part 1 test", part1(part1testInput1), "24176176")
+	//
+	//part1testInput2 := []int{1, 9, 6, 1, 7, 8, 0, 4, 2, 0, 7, 2, 0, 2, 2, 0, 9, 1, 4, 4, 9, 1, 6, 0, 4, 4, 1, 8, 9, 9, 1, 7}
+	//aoc.CheckAnswer("Part 1 test", part1(part1testInput2), "73745418")
+	//
+	//aoc.CheckAnswer("Part 1", part1(input), "63483758")
+
+	//part2testInput1 := []int{0, 3, 0, 3, 6, 7, 3, 2, 5, 7, 7, 2, 1, 2, 9, 4, 4, 0, 6, 3, 4, 9, 1, 5, 6, 5, 4, 7, 4, 6, 6, 4}
+	//aoc.CheckAnswer("Part 2 test", part2(part2testInput1), "84462026")
+
+	aoc.CheckAnswer("Part 2", part2(input), "63483758")
 }
 
 func part1(input []int) string {
+	return doIt(input, 1)
+}
+
+func part2(input []int) string {
+	inputCopies := 10_000
+
+	var realInput []int
+	for i := 0; i < inputCopies; i++ {
+		for _, v := range input {
+			realInput = append(realInput, v)
+		}
+	}
+
+	realInputLength := len(realInput)
+
+	fmt.Printf("real input length: %v\n", realInputLength)
+
+	for phase := 1; phase <= 100; phase++ {
+		patternLength := phase * 4
+		lcm := LCM(len(input), patternLength)
+		repeats := realInputLength / lcm
+		fmt.Printf("phase: %v, lcm: %v, repeats: %v, remainder: %v\n", phase, lcm, repeats, realInputLength-(repeats*lcm))
+
+	}
+	return ":("
+}
+
+func doIt(input []int, inputCopies int) string {
 	basePattern := []int{0, 1, 0, -1}
+	iterations := 100
 
-	current := input
+	var realInput []int
+	for i := 0; i < inputCopies; i++ {
+		for _, v := range input {
+			realInput = append(realInput, v)
+		}
+	}
 
-	for i := 0; i < 100; i++ {
+	realInputLength := len(realInput)
+
+	fmt.Printf("real input length: %v\n", realInputLength)
+
+	allPlusOneIndices := make([][]int, realInputLength)
+	allMinusOneIndices := make([][]int, realInputLength)
+	for i := 0; i < realInputLength; i++ {
+		if i%100 == 0 {
+			println(i)
+		}
+		plusOneIndices, minusOneIndices := getImportantPatternIndices(basePattern, realInputLength, i)
+		allPlusOneIndices[i] = plusOneIndices
+		allMinusOneIndices[i] = minusOneIndices
+	}
+
+	fmt.Printf("allPlusOneIndices length: %v\n", len(allPlusOneIndices))
+
+	current := realInput
+
+	fmt.Println("starting")
+
+	for phase := 0; phase < iterations; phase++ {
+		fmt.Println(phase)
 		var next []int
 		for i := 0; i < len(current); i++ {
-			pattern := updateBasePattern(basePattern, i)
-
-			total := 0
-			for j, v := range current {
-				total += v * pattern[j%len(pattern)]
+			pluses := 0
+			for _, j := range allPlusOneIndices[i] {
+				pluses += current[j]
 			}
-			next = append(next, abs(total%10))
+
+			minuses := 0
+			for _, j := range allMinusOneIndices[i] {
+				minuses += current[j]
+			}
+
+			next = append(next, abs((pluses-minuses)%10))
 		}
 		current = next
+		//fmt.Println(current)
 	}
 
 	return stringify(current)
+}
+
+func getImportantPatternIndices(basePattern []int, max int, i int) ([]int, []int) {
+	pattern := updateBasePattern(basePattern, i)
+	var plusOneIndices []int
+	var minusOneIndices []int
+	for i := 0; i < max; i++ {
+		if pattern[i%len(pattern)] == 1 {
+			plusOneIndices = append(plusOneIndices, i)
+		}
+		if pattern[i%len(pattern)] == -1 {
+			minusOneIndices = append(minusOneIndices, i)
+		}
+	}
+	return plusOneIndices, minusOneIndices
 }
 
 func updateBasePattern(basePattern []int, position int) []int {
@@ -44,7 +131,7 @@ func updateBasePattern(basePattern []int, position int) []int {
 			newPattern = append(newPattern, digit)
 		}
 	}
-	return append(newPattern[1:len(newPattern)], newPattern[0])
+	return append(newPattern[1:], newPattern[0])
 }
 
 func stringify(ints []int) string {
@@ -61,4 +148,25 @@ func abs(i int) int {
 	} else {
 		return -i
 	}
+}
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
 }
