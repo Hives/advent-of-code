a91bf409009f2e3b6cff50fa615f37f9979f13b1
Paul Martin <paul.y.martin@johnlewis.co.uk>
2024-12-12 10:39:56 +0000

    2024 day 12 part 1

---
+++ aoc-2024-kotlin/src/main/kotlin/days/day12/day12.kt
@@ -0,0 +1,55 @@
+package days.day12
+
+import kotlin.system.exitProcess
+import lib.Grid
+import lib.Reader
+import lib.Vector
+import lib.atOrNull
+import lib.cells
+import lib.checkAnswer
+import lib.time
+
+fun main() {
+    val input = Reader("/day12/input.txt").grid()
+    val exampleInput = Reader("/day12/example-1.txt").grid()
+
+    part1(input).checkAnswer(12)
+
+    exitProcess(0)
+
+    time(message = "Part 1") {
+        part1(input)
+    }.checkAnswer(1485656)
+
+    time(message = "Part 2") {
+        part2(input)
+    }.checkAnswer(0)
+}
+
+fun part1(grid: Grid<Char>): Int {
+    val visited = mutableSetOf<Vector>()
+    val regions = mutableListOf<Set<Vector>>()
+    grid.cells().forEach { (point, value) ->
+        if (point !in visited) {
+            val region = mutableSetOf(point)
+            var frontier = listOf(point)
+            while (frontier.isNotEmpty()) {
+                val newFrontier = frontier.flatMap { it.neighbours }.distinct()
+                    .filter { grid.atOrNull(it) == value && it !in region }
+                region += newFrontier
+                frontier = newFrontier
+                visited += newFrontier
+            }
+            regions += region
+        }
+    }
+
+    return regions.sumOf { it.getFenceCost() }
+}
+
+fun part2(input: Grid<Char>): Int {
+    return -1
+}
+
+fun Set<Vector>.getFenceCost() =
+    sumOf { (it.neighbours - this).size } * size
