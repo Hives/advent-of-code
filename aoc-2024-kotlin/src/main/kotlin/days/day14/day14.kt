e3835e6c6140538dd774c8b715ec8b042b45c203
Paul Martin <paul.y.martin@johnlewis.co.uk>
2024-12-14 10:27:50 +0000

    2024 day 14 part 2

--- aoc-2024-kotlin/src/main/kotlin/days/day14/day14.kt
+++ aoc-2024-kotlin/src/main/kotlin/days/day14/day14.kt
@@ -10,36 +10,44 @@ fun main() {
     val input = Reader("/day14/input.txt").strings()
     val exampleInput = Reader("/day14/example-1.txt").strings()
 
+    part2(input)
+
+    exitProcess(0)
 
     time(message = "Part 1") {
         part1(input)
     }.checkAnswer(229980828)
 
-    exitProcess(0)
-
-    time(message = "Part 2") {
+    time(message = "Part 2", iterations = 5, warmUp = 5) {
         part2(input)
-    }.checkAnswer(0)
+    }.checkAnswer(7132)
 }
 
 fun part1(input: List<String>) =
     input.map(::parseInput)
         .map { it.move(100, 101, 103) }
-        .countPointsInQuadrants(101, 103)
+        .countRobotsInQuadrants(101, 103)
         .score()
 
 fun part2(input: List<String>): Int {
-    return -1
+    var robots = input.map(::parseInput)
+    var seconds = 0
+    while (!robots.hasDiagonals(101)) {
+        seconds ++
+        robots = robots.map { it.move(1, 101, 103) }
+    }
+    robots.printy(101, 103)
+    return seconds
 }
 
-fun List<Vector>.countPointsInQuadrants(width: Int, height: Int) =
+fun List<Robot>.countRobotsInQuadrants(width: Int, height: Int) =
     listOf(
         Pair(Vector(0, 0), Vector(width / 2, height / 2)),
         Pair(Vector(width / 2 + 1, 0), Vector(width, height / 2)),
         Pair(Vector(0, height / 2 + 1), Vector(width / 2, height)),
         Pair(Vector(width / 2 + 1, height / 2 + 1), Vector(width, height)),
     ).map { quadrant ->
-        count { point ->
+        count { (point) ->
             point.x >= quadrant.first.x && point.x < quadrant.second.x && point.y >= quadrant.first.y && point.y < quadrant.second.y
         }
     }
@@ -57,7 +65,29 @@ fun parseInput(input: String): Robot {
     }
 }
 
+fun List<Robot>.hasDiagonals(width: Int): Boolean {
+    val centered = filter { it.pos.x == width / 2 + 1 }
+    val points = map(Robot::pos).toSet()
+    return centered.any {
+        List(5) { n -> it.pos + (Vector(-1, 1) * n) }.all { it in points }
+    }
+}
+
+fun List<Robot>.printy(width: Int, height: Int) {
+    val points = map(Robot::pos).toSet()
+    (0..height).forEach { y ->
+        (0..width).forEach { x ->
+            if (Vector(x, y) in points) print("X")
+            else print(".")
+        }
+        println()
+    }
+    println()
+}
+
 data class Robot(val pos: Vector, val vel: Vector) {
-    fun move(seconds: Int, width: Int, height: Int) =
-        (pos + vel * seconds).let { (x, y) -> Vector(x.mod(width), y.mod(height)) }
+    fun move(seconds: Int, width: Int, height: Int): Robot =
+        (pos + vel * seconds).let { (x, y) ->
+            copy(pos = Vector(x.mod(width), y.mod(height)))
+        }
 }
